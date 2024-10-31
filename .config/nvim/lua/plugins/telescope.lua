70f15148a55ef312e3b68f9ee1d106c7cbc287a3
Franco Ruggeri <francoruggeri95@gmail.com>
2024-10-31 23:14:34 +0100

    feat: add telescope

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,21 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+    -- Required dependency
+    { "nvim-lua/plenary.nvim" },
+    -- Improve sorting performance
+    { 
+      "nvim-telescope/telescope-fzf-native.nvim", 
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+    },
+	},
+  config = function()
+    require("telescope").setup()
+
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+  end,
+}
