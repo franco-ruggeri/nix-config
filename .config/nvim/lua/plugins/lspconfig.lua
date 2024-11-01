9f5d2bba1d73616e494375d7c92b7064857f817d
Franco Ruggeri <francoruggeri95@gmail.com>
2024-11-01 02:09:49 +0100

    feat: add lspconfig

---
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,30 @@
+return {
+  -- lspconfig configures the built-in neovim LSP client
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    -- mason is a package manager for LSP servers, among other things
+    { "williamboman/mason.nvim" },
+    -- mason-lspconfig automates the LSP client setup
+    { "williamboman/mason-lspconfig.nvim" },
+  },
+  config = function()
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(event)
+        -- TODO: add keymaps to use LSP functions
+      end,
+    })
+
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = { "lua_ls", "pyright" },
+    })
+
+    -- Automatic LSP client setup for each LSP server installed with Mason.
+    -- Otherwise, we'd need to setup the LSP client explicitly for every LSP server.
+    require("mason-lspconfig").setup_handlers({
+        function (server_name)
+            require("lspconfig")[server_name].setup({}) -- table is required
+        end,
+    })
+  end,
+}
