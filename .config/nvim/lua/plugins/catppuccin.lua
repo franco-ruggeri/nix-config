82d57b0387b54f47e264454161c4f0c1bb821239
Franco Ruggeri <francoruggeri95@gmail.com>
2024-11-26 20:40:23 +0100

    feat: fix colorscheme in neovim

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,7 +1,11 @@
-return { 
-  "catppuccin/nvim", 
-  priority = 1000, 
-  init = function()
-    vim.cmd.colorscheme = "catppuccin"
-  end
+return {
+  "catppuccin/nvim",
+  priority = 1000,
+  opts = {
+    transparent_background = true,
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
