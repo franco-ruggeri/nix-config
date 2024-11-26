1909fc39d3de685075a08d1dcaef4b33f9e9de3c
Franco Ruggeri <francoruggeri95@gmail.com>
2024-11-26 21:47:43 +0100

    feat: add shortcuts for LSP features

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -12,7 +12,22 @@ return {
   config = function()
     vim.api.nvim_create_autocmd("LspAttach", {
       callback = function(event)
-        -- TODO: add keymaps to use LSP functions
+        local map = function(keys, func, desc, mode)
+          mode = mode or 'n'
+          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+        end
+        local telescope = require("telescope.builtin")
+
+        -- Note: you can press <C-t> to jump back
+        map('gd', telescope.lsp_definitions, '[G]oto [D]efinition')
+        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        map('gr', telescope.lsp_references, '[G]oto [R]eferences')
+        map('gI', telescope.lsp_implementations, '[G]oto [I]mplementation')
+        map('<leader>ds', telescope.lsp_document_symbols, '[D]ocument [S]ymbols')
+        map('<leader>ws', telescope.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+        map('<leader>td', telescope.lsp_type_definitions, '[T]ype [D]efinition')
       end,
     })
 
@@ -22,7 +37,7 @@ return {
     })
 
     -- Automatic LSP client setup for each LSP server installed with Mason.
-    -- Otherwise, we'd need to setup the LSP client explicitly for every LSP server.
+    -- Otherwise, we'd need to set up the LSP client explicitly for every LSP server.
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
     require("mason-lspconfig").setup_handlers({
       function (server_name)
