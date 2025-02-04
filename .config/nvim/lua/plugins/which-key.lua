2498d1466f6b74d475cdffa33ae33b248ee7f54e
Franco Ruggeri <francoruggeri95@gmail.com>
2025-02-04 18:31:14 +0100

    fix(nvim): avoid conflict of <D-w> with aerospace

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,5 +1,20 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  opts = {},
+  config = function()
+    local wk = require("which-key")
+
+    -- On macOS, I want to remap <D-w> to <C-w> to have the window functions on <C-w> like on Linux. However:
+    -- - Aerospace uses <C-w>, so I can't just remap <D-w> to <C-w> in Karabiner (system level).
+    -- - MacOS uses <D-w> for closing windows, so I can't just remap <D-w> to <C-w> in Neovim (application level).
+    -- 
+    -- Thus, I use the following trick:
+    -- 1. I remap <D-w> to <M-w> in Karabiner (system level).
+    -- 2. I remap <M-w> to <C-w> in Neovim (application level).
+    if vim.fn.has("macunix") then
+      wk.add({
+        { "<M-w>", proxy = "<C-w>", group = "windows" },
+      })
+    end
+  end,
 }
