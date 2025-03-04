fde68d0a7362f8021a598f60561303dffb29e052
Franco Ruggeri <francoruggeri95@gmail.com>
2025-03-04 22:59:59 +0100

    style(nvim): format

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,22 +1,20 @@
 return {
-  -- lspconfig configures the built-in neovim LSP client
   "neovim/nvim-lspconfig",
   dependencies = {
-    -- mason is a package manager for LSP servers, among other things
-    { "williamboman/mason.nvim" },
-    -- mason-lspconfig automates the LSP client setup
-    { "williamboman/mason-lspconfig.nvim" },
-    -- cmp-nvim-lsp provides extra capabilities for autocompletion
-    { "hrsh7th/cmp-nvim-lsp" },
+    -- Package manager for LSP servers
+    "williamboman/mason.nvim",
+    -- Automates the LSP client setup
+    "williamboman/mason-lspconfig.nvim",
+    -- Provides extra capabilities for autocompletion
+    "hrsh7th/cmp-nvim-lsp",
   },
   config = function()
     vim.api.nvim_create_autocmd("LspAttach", {
       callback = function(event)
         local telescope = require("telescope.builtin")
-
-        -- Note: you can press <C-t> to jump back
         vim.keymap.set("n", "gd", telescope.lsp_definitions, { buffer = event.buf, desc = "[g]oto [d]efinition" })
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = event.buf, desc = "[g]oto [d]eclaration" })
+        vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { buffer = event.buf, desc = "[L]SP [r]ename" })
       end,
     })
 
@@ -30,8 +28,8 @@ return {
     -- Otherwise, we'd need to set up the LSP client explicitly for every LSP server.
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
     require("mason-lspconfig").setup_handlers({
-      function (server_name)
-        require("lspconfig")[server_name].setup({capabilities = capabilities})
+      function(server_name)
+        require("lspconfig")[server_name].setup({ capabilities = capabilities })
       end,
     })
   end,
