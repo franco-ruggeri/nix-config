76b475eec61a7a8f4e393b65f9634ce211a1dd99
Franco Ruggeri <francoruggeri95@gmail.com>
2025-03-04 22:59:07 +0100

    chore(nvim): update tree shortcuts

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -6,9 +6,9 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   keys = {
-    { "<leader>et", "<cmd>NvimTreeToggle<cr>", desc = "[e]xplore - [t]oggle tree" },
-    { "<leader>eb", "<cmd>NvimTreeFindFile<cr>", desc = "[e]xplore - find [b]uffer in tree" },
-    { "<leader>ec", "<cmd>NvimTreeCollapse<cr>", desc = "[e]xplore - [c]ollapse tree" },
+    { "<leader>ee", "<cmd>NvimTreeToggle<cr>", desc = "[e]xplore toggle tree" },
+    { "<leader>eb", "<cmd>NvimTreeFindFile<cr>", desc = "[e]xplore find [b]uffer in tree" },
+    { "<leader>ec", "<cmd>NvimTreeCollapse<cr>", desc = "[e]xplore [c]ollapse tree" },
   },
   opts = {},
 }
