0adc9012a27df3510a1c666595a12ac67d4e2b25
Franco Ruggeri <francoruggeri95@gmail.com>
2025-03-05 01:10:41 +0100

    chore(nvim): simplify cmp config

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,52 +1,43 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
     "neovim/nvim-lspconfig",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
     "hrsh7th/cmp-buffer",
-	},
-	config = function()
-		local cmp = require("cmp")
+  },
+  config = function()
+    local cmp = require("cmp")
 
     -- Completion in text
     cmp.setup({
-      mapping = cmp.mapping.preset.insert({
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-y>"] = cmp.mapping.confirm { select = true },
-        ["<C-Space>"] = cmp.mapping.complete {},
-      }),
+      mapping = cmp.mapping.preset.insert(),
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "path" },
-        { name = "cmdline" },
       }, {
-          { name = "buffer" },
-        }
-      )
+        { name = "buffer" },
+      }),
     })
 
     -- Completion in search mode
-    cmp.setup.cmdline({ '/', '?' }, {
+    cmp.setup.cmdline({ "/", "?" }, {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
-        { name = 'buffer' }
+        { name = "buffer" }
       }
     })
 
     -- Completion in command-line mode
-    cmp.setup.cmdline(':', {
+    cmp.setup.cmdline(":", {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
-        { name = 'path' }
+        { name = "path" }
       }, {
-          { name = 'cmdline' }
-        }),
+        { name = "cmdline" }
+      }),
     })
   end,
 }
