44e3abd370bae614eb79bdb2214e37ac4b927b2f
Franco Ruggeri <francoruggeri95@gmail.com>
2025-03-05 16:20:27 +0100

    feat(nvim): add null-ls (format + lint)

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,36 +1,46 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    -- Package manager for LSP servers
-    "williamboman/mason.nvim",
-    -- Automates the LSP client setup
-    "williamboman/mason-lspconfig.nvim",
-    -- Provides extra capabilities for autocompletion
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  config = function()
-    vim.api.nvim_create_autocmd("LspAttach", {
-      callback = function(event)
-        local telescope = require("telescope.builtin")
-        vim.keymap.set("n", "gd", telescope.lsp_definitions, { buffer = event.buf, desc = "[g]oto [d]efinition" })
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = event.buf, desc = "[g]oto [d]eclaration" })
-        vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { buffer = event.buf, desc = "[L]SP [r]ename" })
-      end,
-    })
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim", -- package manager for LSP servers
+		"williamboman/mason-lspconfig.nvim", -- automates the LSP client setup
+		"hrsh7th/cmp-nvim-lsp", -- provides extra capabilities for autocompletion
+	},
+	config = function()
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(event)
+				local telescope = require("telescope.builtin")
+				vim.keymap.set(
+					"n",
+					"gd",
+					telescope.lsp_definitions,
+					{ buffer = event.buf, desc = "[g]oto [d]efinition" }
+				)
+				vim.keymap.set(
+					"n",
+					"gD",
+					vim.lsp.buf.declaration,
+					{ buffer = event.buf, desc = "[g]oto [d]eclaration" }
+				)
+				vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { buffer = event.buf, desc = "[L]SP [r]ename" })
+				vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { buffer = event.buf, desc = "[L]SP [f]ormat" })
+				vim.keymap.set("v", "=", vim.lsp.buf.format, { buffer = event.buf })
 
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = {},
-      automatic_installation = false,
-    })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					buffer = event.buf,
+					callback = function()
+						vim.lsp.buf.format({ async = false })
+					end,
+				})
+			end,
+		})
 
-    -- Automatic LSP client setup for each LSP server installed with Mason.
-    -- Otherwise, we'd need to set up the LSP client explicitly for every LSP server.
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
-    require("mason-lspconfig").setup_handlers({
-      function(server_name)
-        require("lspconfig")[server_name].setup({ capabilities = capabilities })
-      end,
-    })
-  end,
+		-- Automatic configuration of LSP client for each LSP server installed with Mason.
+		require("mason-lspconfig").setup()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		require("mason-lspconfig").setup_handlers({
+			function(server_name)
+				require("lspconfig")[server_name].setup({ capabilities = capabilities })
+			end,
+		})
+	end,
 }
