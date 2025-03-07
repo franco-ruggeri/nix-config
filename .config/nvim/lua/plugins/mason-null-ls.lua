493c012b76f23c983e3f3ca32a6219ff158a0f13
Franco Ruggeri <francoruggeri95@gmail.com>
2025-03-07 23:08:03 +0100

    chore(nvim): drop pylsp plugins

--- .config/nvim/lua/plugins/mason-null-ls.lua
+++ .config/nvim/lua/plugins/mason-null-ls.lua
@@ -6,20 +6,11 @@ return {
 		"nvim-lua/plenary.nvim", -- required
 	},
 	config = function()
-		local null_ls = require("null-ls")
 		local mason_null_ls = require("mason-null-ls")
 		mason_null_ls.setup({
 			ensure_installed = {},
 			automatic_installation = false,
-			-- TODO: this is work in progress
-			handlers = {
-				function(source_name, methods)
-					mason_null_ls.default_setup(source_name, methods)
-				end,
-				pylint = function(source_name, methods)
-					null_ls.register(null_ls.builtins.diagnostics.pylint)
-				end,
-			},
+			handlers = {},
 		})
 	end,
 }
