35934a05efb131ed5e3e67e926d8c0ca7a2eb2da
Franco Ruggeri <francoruggeri95@gmail.com>
2025-04-19 00:16:11 +0200

    feat(nvim): set up helm-ls

--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,43 +1,53 @@
 local utils = require("utils")
 
 return {
-	"williamboman/mason-lspconfig.nvim",
-	dependencies = {
-		"williamboman/mason.nvim", -- package manager for LSP servers
-		"neovim/nvim-lspconfig",
-		"hrsh7th/cmp-nvim-lsp", -- provides extra capabilities for autocompletion
-		"artemave/workspace-diagnostics.nvim",
-		"nvim-telescope/telescope.nvim", -- for LSP pickers (used in on_attach)
-	},
-	config = function()
-		local lspconfig = require("lspconfig")
-		local mason_lspconfig = require("mason-lspconfig")
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",       -- package manager for LSP servers
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",          -- provides extra capabilities for autocompletion
+    "artemave/workspace-diagnostics.nvim",
+    "nvim-telescope/telescope.nvim", -- for LSP pickers (used in on_attach)
+  },
+  config = function()
+    local lspconfig = require("lspconfig")
+    local mason_lspconfig = require("mason-lspconfig")
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		mason_lspconfig.setup()
-		mason_lspconfig.setup_handlers({
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-					on_attach = utils.lsp.on_attach,
-				})
-			end,
-
-			pylsp = function()
-				lspconfig.pylsp.setup({
-					capabilities = capabilities,
-					on_attach = utils.lsp.on_attach,
-					settings = {
-						pylsp = {
-							plugins = {
-								pycodestyle = {
-									enabled = false,
-								},
-							},
-						},
-					},
-				})
-			end,
-		})
-	end,
+    mason_lspconfig.setup()
+    mason_lspconfig.setup_handlers({
+      function(server_name)
+        lspconfig[server_name].setup({
+          capabilities = capabilities,
+          on_attach = utils.lsp.on_attach,
+        })
+      end,
+      pylsp = function()
+        lspconfig.pylsp.setup({
+          capabilities = capabilities,
+          on_attach = utils.lsp.on_attach,
+          settings = {
+            pylsp = {
+              plugins = {
+                pycodestyle = {
+                  enabled = false,
+                },
+              },
+            },
+          },
+        })
+      end,
+      helm_ls = function()
+        lspconfig.helm_ls.setup({
+          settings = {
+            ["helm-ls"] = {
+              yamlls = {
+                path = "yaml-language-server",
+              }
+            }
+          }
+        })
+      end,
+    })
+  end,
 }
