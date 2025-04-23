819092332af80ef8e1d7bc3047ea656f37e7babd
Franco Ruggeri <francoruggeri95@gmail.com>
2025-04-23 16:40:17 +0200

    Revert "chore(nvim): remove custom handler of pylsp"
    This reverts commit 85765a0fdb71e498108fd39c356cc6dab705bdc0.

--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,28 +1,43 @@
 local utils = require("utils")
 
 return {
-  "williamboman/mason-lspconfig.nvim",
-  dependencies = {
-    "williamboman/mason.nvim",       -- package manager for LSP servers
-    "neovim/nvim-lspconfig",
-    "hrsh7th/cmp-nvim-lsp",          -- provides extra capabilities for autocompletion
-    "artemave/workspace-diagnostics.nvim",
-    "nvim-telescope/telescope.nvim", -- for LSP pickers (used in on_attach)
-    "towolf/vim-helm",               -- required to make helm-ls work
-  },
-  config = function()
-    local lspconfig = require("lspconfig")
-    local mason_lspconfig = require("mason-lspconfig")
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason.nvim", -- package manager for LSP servers
+		"neovim/nvim-lspconfig",
+		"hrsh7th/cmp-nvim-lsp", -- provides extra capabilities for autocompletion
+		"artemave/workspace-diagnostics.nvim",
+		"nvim-telescope/telescope.nvim", -- for LSP pickers (used in on_attach)
+		"towolf/vim-helm", -- required to make helm-ls work
+	},
+	config = function()
+		local lspconfig = require("lspconfig")
+		local mason_lspconfig = require("mason-lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    mason_lspconfig.setup()
-    mason_lspconfig.setup_handlers({
-      function(server_name)
-        lspconfig[server_name].setup({
-          capabilities = capabilities,
-          on_attach = utils.lsp.on_attach,
-        })
-      end,
-    })
-  end,
+		mason_lspconfig.setup()
+		mason_lspconfig.setup_handlers({
+			function(server_name)
+				lspconfig[server_name].setup({
+					capabilities = capabilities,
+					on_attach = utils.lsp.on_attach,
+				})
+			end,
+			pylsp = function()
+				lspconfig.pylsp.setup({
+					capabilities = capabilities,
+					on_attach = utils.lsp.on_attach,
+					settings = {
+						pylsp = {
+							plugins = {
+								pycodestyle = {
+									enabled = false,
+								},
+							},
+						},
+					},
+				})
+			end,
+		})
+	end,
 }
