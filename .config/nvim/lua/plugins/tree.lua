15e325bf539f7fde3c602ae6e234e9e164b389c9
Franco Ruggeri <francoruggeri95@gmail.com>
2025-04-23 19:21:17 +0200

    feat(nvim): add shortcut to find buffer in tree

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -7,6 +7,7 @@ return {
 	},
 	config = function()
 		require("nvim-tree").setup({})
-		vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "[e]xplore tree" })
+		vim.keymap.set("n", "<leader>et", "<cmd>NvimTreeToggle<cr>", { desc = "[e]xplore [t]oggle tree" })
+		vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFile<cr>", { desc = "[e]xplore find [b]uffer in tree" })
 	end,
 }
