66a894516469e53237de1cd9a5a911f0ebcb2ae0
Franco Ruggeri <francoruggeri95@gmail.com>
2025-05-15 16:30:39 +0200

    chore(nvim): simplify trouble keymaps

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -4,24 +4,9 @@ return {
 	cmd = "Trouble",
 	keys = {
 		{
-			"<leader>xx",
+			"<leader>x",
 			"<cmd>Trouble diagnostics toggle focus=true<cr>",
-			desc = "trouble - diagnostics (global)",
-		},
-		{
-			"<leader>xb",
-			"<cmd>Trouble diagnostics toggle focus=true filter.buf=0<cr>",
-			desc = "trouble - diagnostics [b]uffer",
-		},
-		{
-			"<leader>xl",
-			"<cmd>Trouble loclist toggle focus=true<cr>",
-			desc = "trouble - [l]ocation list",
-		},
-		{
-			"<leader>xq",
-			"<cmd>Trouble qflist toggle focus=true<cr>",
-			desc = "trouble - [q]uickfix list",
+			desc = "trouble diagnostics",
 		},
 	},
 }
