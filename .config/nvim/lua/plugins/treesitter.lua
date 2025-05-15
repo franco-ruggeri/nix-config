b73f09a00014e9226f3152d155ad730f738494ff
Franco Ruggeri <francoruggeri95@gmail.com>
2025-05-15 11:11:04 +0200

    chore(nvim): remove unnecessary treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,6 @@ return {
 	main = "nvim-treesitter.configs",
 	opts = {
 		ensure_installed = "all",
-		auto_install = true,
 		highlight = { enable = true, disable = { "latex" } },
 		indent = { enable = true, disable = { "latex" } },
 	},
