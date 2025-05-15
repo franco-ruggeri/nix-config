66b533b167e19ef02761a9f7c7e36756ab3ad0fb
Franco Ruggeri <francoruggeri95@gmail.com>
2025-05-15 11:11:25 +0200

    feat(nvim): enable transparent background

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,7 +1,9 @@
 return {
 	"catppuccin/nvim",
 	priority = 1000,
-	opts = {},
+	opts = {
+		transparent_background = true,
+	},
 	config = function(_, opts)
 		require("catppuccin").setup(opts)
 		vim.cmd.colorscheme("catppuccin")
