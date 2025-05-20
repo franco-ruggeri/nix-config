dbd20cbb52aa39811cc46c24616cbbceed9d4152
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-21 01:35:30 +0200

    fix(nvim): use python null-ls only in venv

--- .config/nvim/lua/plugins/mason-null-ls.lua
+++ .config/nvim/lua/plugins/mason-null-ls.lua
@@ -1,48 +1,56 @@
+local function register_in_venv(type, tool)
+	-- Check if a venv is active
+	if vim.env.VIRTUAL_ENV == nil then
+		return
+	end
+
+	-- Check if source is installed in venv
+	local bin_path = vim.env.VIRTUAL_ENV .. "/bin/" .. tool
+	if vim.fn.executable(bin_path) == 0 then
+		return
+	end
+
+	-- Register the source with the venv command
+	local null_ls = require("null-ls")
+	null_ls.register(null_ls.builtins[type][tool].with({
+		command = bin_path,
+	}))
+end
+
 return {
-  "jay-babu/mason-null-ls.nvim",
-  dependencies = {
-    "williamboman/mason.nvim",       -- package manager for linters and formatters
-    "nvimtools/none-ls.nvim",
-    "nvim-lua/plenary.nvim",         -- required
-    "nvim-telescope/telescope.nvim", -- for LSP pickers (used in on_attach)
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    local mason_null_ls = require("mason-null-ls")
-    local is_venv = vim.env.VIRTUAL_ENV ~= nil
+	"jay-babu/mason-null-ls.nvim",
+	dependencies = {
+		"williamboman/mason.nvim", -- package manager for linters and formatters
+		"nvimtools/none-ls.nvim",
+		"nvim-lua/plenary.nvim", -- required
+		"nvim-telescope/telescope.nvim", -- for LSP pickers (used in on_attach)
+	},
+	config = function()
+		local mason_null_ls = require("mason-null-ls")
 
-    mason_null_ls.setup({
-      ensure_installed = {},
-      automatic_installation = false,
-      handlers = {
-        -- Default handler: nothing special, just use the default setup
-        function(source_name, methods)
-          mason_null_ls.default_setup(source_name, methods)
-        end,
+		mason_null_ls.setup({
+			ensure_installed = {},
+			automatic_installation = false,
+			handlers = {
+				-- Default handler: nothing special, just use the default setup
+				function(source_name, methods)
+					mason_null_ls.default_setup(source_name, methods)
+				end,
 
-        -- Use code quality tools only if installed in the venv.
-        -- Use the binary from the venv, so that:
-        -- - Tools see installed packages.
-        -- - Tools use the config in pyproject.toml.
-        pylint = function()
-          if is_venv then
-            local overrides = { command = vim.env.VIRTUAL_ENV .. "/bin/pylint" }
-            null_ls.register(null_ls.builtins.diagnostics.pylint.with(overrides))
-          end
-        end,
-        mypy = function()
-          if is_venv then
-            local overrides = { command = vim.env.VIRTUAL_ENV .. "/bin/mypy" }
-            null_ls.register(null_ls.builtins.diagnostics.mypy.with(overrides))
-          end
-        end,
-        black = function()
-          if is_venv then
-            local overrides = { command = vim.env.VIRTUAL_ENV .. "/bin/black" }
-            null_ls.register(null_ls.builtins.formatting.black.with(overrides))
-          end
-        end,
-      },
-    })
-  end,
+				-- Use code quality tools only if installed in the venv.
+				-- Use the binary from the venv, so that:
+				-- - Tools see installed packages.
+				-- - Tools use the config in pyproject.toml.
+				pylint = function()
+					register_in_venv("diagnostics", "pylint")
+				end,
+				mypy = function()
+					register_in_venv("diagnostics", "mypy")
+				end,
+				black = function()
+					register_in_venv("formatting", "black")
+				end,
+			},
+		})
+	end,
 }
