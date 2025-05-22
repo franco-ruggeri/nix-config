85cd5160cf1377c330aeb1b961a3639602bfefc9
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-22 23:33:48 +0200

    feat(nvim): include source in diagnostics

--- .config/nvim/lua/utils/diagnostics.lua
+++ .config/nvim/lua/utils/diagnostics.lua
@@ -1,20 +1,32 @@
 local M = {}
 
-M.toggle = function()
-  local info = vim.fn.getqflist({ title = 1, winid = 1 })
-  local title = info.title
-  local window_id = info.winid
+M.refresh = function()
+	local diagnostics = vim.diagnostic.get()
 
-  if title == "Diagnostics" and window_id ~= 0 then
-    vim.cmd("cclose")
-  else
-    vim.diagnostic.setqflist()
-    vim.cmd("copen") -- open anyway, even if there are no diagnostics
-  end
+	-- Include source in the message
+	for i = 1, #diagnostics do
+		local d = diagnostics[i]
+		if d.source then
+			d.message = string.format("%s [%s]", d.message, d.source)
+		end
+	end
+
+	-- Send diagnostics to quickfix list
+	local quickfix_items = vim.diagnostic.toqflist(diagnostics)
+	vim.fn.setqflist({}, "r", { items = quickfix_items, title = "Diagnostics" })
 end
 
-M.refresh = function()
-  vim.diagnostic.setqflist({ open = false })
+M.toggle = function()
+	local info = vim.fn.getqflist({ title = 1, winid = 1 })
+	local title = info.title
+	local window_id = info.winid
+
+	if title == "Diagnostics" and window_id ~= 0 then
+		vim.cmd("cclose")
+	else
+		M.refresh()
+		vim.cmd("copen")
+	end
 end
 
 return M
