7e274f97613a6c5ee39ad5541b047c8d8b940178
Franco Ruggeri <francoruggeri95@gmail.com>
2025-05-23 01:31:22 +0200

    feat(nvim): add source and code in diagnostics

--- .config/nvim/lua/utils/diagnostics.lua
+++ .config/nvim/lua/utils/diagnostics.lua
@@ -1,19 +1,49 @@
 local M = {}
 
-M.refresh = function()
-	local diagnostics = vim.diagnostic.get()
+local function format_quickfix_list(info)
+	-- Default format extended with source and symbol
+	local format = "%s|%d col %d-%d %s| %s%s%s"
 
-	-- Include source in the message
-	for i = 1, #diagnostics do
-		local d = diagnostics[i]
-		if d.source then
-			d.message = string.format("%s [%s]", d.message, d.source)
-		end
+	local items = vim.fn.getqflist({ id = info.id, items = 0 }).items
+	local items_str = {}
+	for i = info.start_idx, info.end_idx do
+		local item = items[i]
+		local source = item.user_data.source
+		local code = item.user_data.code
+		local item_str = format:format(
+			vim.fn.bufname(item.bufnr),
+			item.lnum,
+			item.col,
+			item.end_col,
+			item.type,
+			item.text,
+			source and (" [%s]"):format(source) or "",
+			code and (" (%s)"):format(code) or ""
+		)
+		table.insert(items_str, item_str)
 	end
 
-	-- Send diagnostics to quickfix list
+	return items_str
+end
+
+M.refresh = function()
+	local diagnostics = vim.diagnostic.get()
 	local quickfix_items = vim.diagnostic.toqflist(diagnostics)
-	vim.fn.setqflist({}, "r", { items = quickfix_items, title = "Diagnostics" })
+	local n_items = #diagnostics
+
+	for i = 1, n_items do
+		local d = diagnostics[i]
+		quickfix_items[i].user_data = {
+			source = d.source,
+			code = d.code,
+		}
+	end
+
+	vim.fn.setqflist({}, "r", {
+		items = quickfix_items,
+		title = "Diagnostics",
+		quickfixtextfunc = format_quickfix_list,
+	})
 end
 
 M.toggle = function()
