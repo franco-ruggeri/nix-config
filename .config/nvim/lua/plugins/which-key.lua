e5a335eaf46938f1a65721a7331d152900ff28f9
Franco Ruggeri <francoruggeri95@gmail.com>
2025-05-23 01:38:11 +0200

    chore(nvim): remove remaps for macOS

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,22 +1,5 @@
-local utils = require("utils")
-
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
-	config = function()
-		local wk = require("which-key")
-
-		-- On macOS, I want to remap <D-w> to <C-w> to have the window functions on <C-w> like on Linux. However:
-		-- - Aerospace uses <C-w>, so I can't just remap <D-w> to <C-w> in Karabiner (system level).
-		-- - MacOS uses <D-w> for closing windows, so I can't just remap <D-w> to <C-w> in Neovim (application level).
-		--
-		-- Thus, I use the following trick:
-		-- 1. I remap <D-w> to <M-w> in Karabiner (system level).
-		-- 2. I remap <M-w> to <C-w> in Neovim (application level).
-		if utils.os.is_macos() then
-			wk.add({
-				{ "<M-w>", proxy = "<C-w>", group = "windows" },
-			})
-		end
-	end,
+	opts = {},
 }
