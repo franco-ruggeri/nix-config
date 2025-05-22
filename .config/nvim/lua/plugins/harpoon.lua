0e1bea7f7b7210df2dc29b1154c1268c9e892df4
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-22 18:09:26 +0200

    chore(nvim): update harpoon shortcuts

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,39 +1,39 @@
 return {
-	"ThePrimeagen/harpoon",
-	branch = "harpoon2",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	opts = {
-		settings = {
-			save_on_toggle = true,
-			save_on_ui_close = true,
-		},
-	},
-	config = function(_, opts)
-		local harpoon = require("harpoon")
-		harpoon:setup(opts)
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    settings = {
+      save_on_toggle = true,
+      save_on_ui_close = true,
+    },
+  },
+  config = function(_, opts)
+    local harpoon = require("harpoon")
+    harpoon:setup(opts)
 
-		vim.keymap.set("n", "<leader>ha", function()
-			harpoon:list():add()
-		end, { desc = "[h]arpoon [a]dd file" })
+    vim.keymap.set("n", "<leader>ha", function()
+      harpoon:list():add()
+    end, { desc = "[h]arpoon [a]dd file" })
 
-		vim.keymap.set("n", "<leader>hh", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end, { desc = "[h]arpoon toggle menu" })
+    vim.keymap.set("n", "<leader>hh", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = "[h]arpoon toggle menu" })
 
-		vim.keymap.set("n", "<leader>hp", function()
-			harpoon:list():prev()
-		end, { desc = "[h]arpoon [p]revious buffer" })
+    vim.keymap.set("n", "<leader>hp", function()
+      harpoon:list():prev()
+    end, { desc = "[h]arpoon [p]revious buffer" })
 
-		vim.keymap.set("n", "<leader>hn", function()
-			harpoon:list():next()
-		end, { desc = "[h]arpoon [n]ext buffer" })
+    vim.keymap.set("n", "<leader>hn", function()
+      harpoon:list():next()
+    end, { desc = "[h]arpoon [n]ext buffer" })
 
-		for i = 1, 9 do
-			vim.keymap.set("n", "<leader>h" .. i, function()
-				harpoon:list():select(i)
-			end, { desc = "[h]arpoon [" .. i .. "]-th buffer" })
-		end
-	end,
+    for i = 1, 9 do
+      vim.keymap.set("n", string.format("<M-%d>", i), function()
+        harpoon:list():select(i)
+      end, { noremap = true, desc = string.format("[h]arpoon [%d]-th buffer", i) })
+    end
+  end,
 }
