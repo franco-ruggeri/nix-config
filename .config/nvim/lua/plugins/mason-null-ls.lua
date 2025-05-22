46788a8093ce8d99d097f6dc2c64b873412da515
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-22 15:13:08 +0200

    feat(nvim): add workspace and buffer diagnostic modes

--- .config/nvim/lua/plugins/mason-null-ls.lua
+++ .config/nvim/lua/plugins/mason-null-ls.lua
@@ -1,56 +1,177 @@
-local function register_in_venv(type, tool)
-	-- Check if a venv is active
-	if vim.env.VIRTUAL_ENV == nil then
-		return
-	end
+-- General notes:
+-- --------------------
+-- For buffer linters, we use:
+-- * multiple_files=false: With this flag, the generator does not clear diagnostics and keeps diagnostics of the open buffers.
+--    Otherwise, there would be a problem when you switch to an already-open file (existing buffer). Specifically:
+--    * Switching to a buffer doesn't trigger the LSP server to publish diagnostics.
+--    * Therefore, no diagnostics would be shown until the buffer changes (trigger).
+-- * method=DIAGNOSTICS: This method lints on buffer change. This is nice to see updated diagnostics while you work.
+--
+-- For workspace linters, we use:
+-- * multiple_files=true: With this flag, the generator clears previous diagnostics to avoid duplicates.
+-- * method=DIAGNOSTICS_ON_SAVE: This method lints only on save. This is important because:
+--    * Since a workspace linter lints disk files, not buffers, we want buffers and disk files to be synced.
+--      Otherwise, we would have outdated diagnostics shown for the updated buffers.
+--    * It is an expensive operations, so we don't want to do it on every change.
+-- --------------------
 
-	-- Check if source is installed in venv
-	local bin_path = vim.env.VIRTUAL_ENV .. "/bin/" .. tool
-	if vim.fn.executable(bin_path) == 0 then
-		return
-	end
+-- For Python linters, if in Python venv...
+local python_env = {}
+local python_prefer_local = nil
+if vim.env.VIRTUAL_ENV then
+  -- ... set PYTHONPATH so that Mason packages can see Python packages installed in venv
+  python_env = { PYTHONPATH = vim.fn.glob(vim.env.VIRTUAL_ENV .. "/lib/python*/site-packages") }
+  -- ... use venv binary over Mason binary when linter is installed in venv
+  python_prefer_local = vim.env.VIRTUAL_ENV .. "/bin"
+end
 
-	-- Register the source with the venv command
-	local null_ls = require("null-ls")
-	null_ls.register(null_ls.builtins[type][tool].with({
-		command = bin_path,
-	}))
+local linters_workspace = {}
+local linters_buffer = {}
+local workspace_mode = false
+
+local function toggle_workspace_mode()
+  local null_ls = require("null-ls")
+
+  workspace_mode = not workspace_mode
+  null_ls.reset_sources()
+
+  local linters = workspace_mode and linters_workspace or linters_buffer
+  for _, linter in ipairs(linters) do
+    null_ls.register(linter)
+  end
+  vim.diagnostic.reset()
+end
+
+local function add_pylint_buffer()
+  local linter = require("null-ls").builtins.diagnostics.pylint.with({
+    env = python_env,
+    prefer_local = python_prefer_local,
+  })
+  table.insert(linters_buffer, linter)
+end
+
+local function add_pylint_workspace()
+  local null_ls = require("null-ls")
+  local helpers = require("null-ls.helpers")
+
+  local generator_opts = vim.tbl_extend("force", null_ls.builtins.diagnostics.pylint.generator.opts, {
+    -- Multiple files so that every linting clears previous diagnostics. Otherwise, diagnostics would be duplicated.
+    multiple_files = true,
+    -- Same as default, but adding filename [*]. Otherwise, all the diagnostics would be assigned to the current buffer.
+    on_output = helpers.diagnostics.from_json({
+      attributes = {
+        row = "line",
+        col = "column",
+        code = "symbol",
+        severity = "type",
+        message = "message",
+        message_id = "message-id",
+        symbol = "symbol",
+        source = "pylint",
+        filename = "path", -- [*] additional diagnostic attribute
+      },
+      severities = {
+        convention = helpers.diagnostics.severities["information"],
+        refactor = helpers.diagnostics.severities["information"],
+      },
+      offsets = {
+        col = 1,
+        end_col = 1,
+      },
+    }),
+  })
+
+  local linter = null_ls.builtins.diagnostics.pylint.with({
+    method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+    generator_opts = generator_opts,
+    env = python_env,
+    prefer_local = python_prefer_local,
+    args = {
+      -- Defaults
+      "--output-format",
+      "json",
+      -- Lint the whole workspace
+      "$ROOT",
+    },
+  })
+
+  table.insert(linters_workspace, linter)
+end
+
+local function add_mypy_buffer()
+  local null_ls = require("null-ls")
+
+  local generator_opts = vim.tbl_extend("force", null_ls.builtins.diagnostics.mypy.generator.opts, {
+    multiple_files = false, -- the default was true, strangely, see general notes
+  })
+
+  local linter = null_ls.builtins.diagnostics.mypy.with({
+    env = python_env,
+    prefer_local = python_prefer_local,
+    generator_opts = generator_opts,
+  })
+
+  table.insert(linters_buffer, linter)
+end
+
+local function add_mypy_workspace()
+  local null_ls = require("null-ls")
+
+  local linter = null_ls.builtins.diagnostics.mypy.with({
+    method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+    env = python_env,
+    prefer_local = python_prefer_local,
+    args = {
+      -- Defaults
+      "--hide-error-codes",
+      "--hide-error-context",
+      "--no-color-output",
+      "--show-absolute-path",
+      "--show-column-numbers",
+      "--show-error-codes",
+      "--no-error-summary",
+      "--no-pretty",
+      -- Lint the whole workspace
+      "$ROOT",
+    },
+    -- Since we don't lint the buffer, to_temp_file isn't necessary
+    to_temp_file = false,
+  })
+
+  table.insert(linters_workspace, linter)
 end
 
 return {
-	"jay-babu/mason-null-ls.nvim",
-	dependencies = {
-		"williamboman/mason.nvim", -- package manager for linters and formatters
-		"nvimtools/none-ls.nvim",
-		"nvim-lua/plenary.nvim", -- required
-		"nvim-telescope/telescope.nvim", -- for LSP pickers (used in on_attach)
-	},
-	config = function()
-		local mason_null_ls = require("mason-null-ls")
+  "jay-babu/mason-null-ls.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",       -- package manager for linters and formatters
+    "nvimtools/none-ls.nvim",
+    "nvim-lua/plenary.nvim",         -- required
+    "nvim-telescope/telescope.nvim", -- for LSP pickers (used in on_attach)
+  },
+  config = function()
+    local mason_null_ls = require("mason-null-ls")
 
-		mason_null_ls.setup({
-			ensure_installed = {},
-			automatic_installation = false,
-			handlers = {
-				-- Default handler: nothing special, just use the default setup
-				function(source_name, methods)
-					mason_null_ls.default_setup(source_name, methods)
-				end,
+    mason_null_ls.setup({
+      ensure_installed = {},
+      automatic_installation = false,
+      handlers = {
+        -- Default handler: nothing special, just use the default setup
+        function(source_name, methods)
+          mason_null_ls.default_setup(source_name, methods)
+        end,
+        -- Custom handlers
+        pylint = function()
+          add_pylint_buffer()
+          add_pylint_workspace()
+        end,
+        mypy = function()
+          add_mypy_buffer()
+          add_mypy_workspace()
+        end,
+      },
+    })
 
-				-- Use code quality tools only if installed in the venv.
-				-- Use the binary from the venv, so that:
-				-- - Tools see installed packages.
-				-- - Tools use the config in pyproject.toml.
-				pylint = function()
-					register_in_venv("diagnostics", "pylint")
-				end,
-				mypy = function()
-					register_in_venv("diagnostics", "mypy")
-				end,
-				black = function()
-					register_in_venv("formatting", "black")
-				end,
-			},
-		})
-	end,
+    vim.keymap.set("n", "<leader>xm", toggle_workspace_mode, { desc = "diagnostics [m]ode toggle" })
+  end,
 }
