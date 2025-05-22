a0068b074ba7cd1fb39a92789e528642b65ebd18
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-22 23:33:28 +0200

    feat(nvim): add basic layout

--- .config/nvim/lua/core/autocommands.lua
+++ .config/nvim/lua/core/autocommands.lua
@@ -10,6 +10,14 @@ M.setup = function()
 		end,
 	})
 
+	vim.api.nvim_create_autocmd("LspAttach", {
+		desc = "Set keymaps and autocommands",
+		callback = function(args)
+			utils.lsp.set_keymaps(args.buf)
+			utils.lsp.set_autocommands(args.buf)
+		end,
+	})
+
 	vim.api.nvim_create_autocmd("DiagnosticChanged", {
 		desc = "Update diagnostics quickfix list",
 		callback = function()
@@ -17,11 +25,20 @@ M.setup = function()
 		end,
 	})
 
-	vim.api.nvim_create_autocmd("LspAttach", {
-		desc = "Set keymaps and autocommands",
-		callback = function(args)
-			utils.lsp.set_keymaps(args.buf)
-			utils.lsp.set_autocommands(args.buf)
+	-- TODO: need to set width of loc list (right)
+	vim.api.nvim_create_autocmd("BufWinEnter", {
+		desc = "Set layout",
+		callback = function()
+			local window_id = vim.api.nvim_get_current_win()
+			local window_info = vim.fn.getwininfo(window_id)[1]
+
+			if window_info.quickfix == 1 then
+				if window_info.loclist == 1 then
+					vim.cmd("wincmd L") -- location list --> right
+				else
+					vim.cmd("wincmd J") -- quickfix list --> bottom
+				end
+			end
 		end,
 	})
 end
