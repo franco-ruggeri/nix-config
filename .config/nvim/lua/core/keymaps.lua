bc2f4e69c6d65346d3d5c2a7af16f0e1a8cb48ab
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-22 19:30:29 +0200

    feat(nvim): use trouble for diagnostics

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,14 +1,9 @@
-local utils = require("utils")
-
 local M = {}
 
 M.setup = function()
 	vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlights" })
 	vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-	vim.keymap.set("n", "<M-n>", "<cmd>cnext<cr>", { desc = "Quickfix list next" })
-	vim.keymap.set("n", "<M-p>", "<cmd>cprevious<cr>", { desc = "Quickfix list previous" })
 	vim.keymap.set("n", "<leader>e", "<cmd>Explore<cr>", { desc = "[e]xplore with netrw" })
-	vim.keymap.set("n", "<leader>xx", utils.diagnostics.toggle, { desc = "diagnostics quickfix list toggle" })
 	vim.keymap.set("n", "<leader>ldr", vim.diagnostic.reset, { desc = "[L]SP [d]iagnostics [r]eset" })
 
 	-- Disable arrow keys
--- .config/nvim/lua/utils/init.lua
+++ .config/nvim/lua/utils/init.lua
@@ -2,6 +2,5 @@ local M = {}
 
 M.lsp = require("utils.lsp")
 M.os = require("utils.os")
-M.diagnostics = require("utils.diagnostics")
 
 return M
