0c1b0570b9adb706f7ec53d1f9667191fe440ec2
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-23 22:43:53 +0200

    feat(nvim): integrate LSP operations with telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -16,5 +16,18 @@ return {
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[f]ind [f]ile" })
 		vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "[f]ind [g]it file" })
 		vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "[f]ind [s]tring" })
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			desc = "Bind LSP functions to Telescope",
+			callback = function(args)
+				vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = args.buf, desc = "[g]oto [d]efinition" })
+				vim.keymap.set(
+					{ "n", "x" },
+					"grr",
+					builtin.lsp_references,
+					{ buffer = args.buf, desc = "[g]oto [r]eferences" }
+				)
+			end,
+		})
 	end,
 }
