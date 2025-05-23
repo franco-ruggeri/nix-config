71e7b985c48872446c8ff05a22045cf38ea832b0
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-23 18:11:19 +0200

    feat(nvim): use Trouble for gO (LSP document symbols)

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,14 +1,27 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {},
+	event = "LspAttach",
 	cmd = "Trouble",
 	keys = {
 		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics toggle" },
+		{ "<M-n>", "<Cmd>Trouble diagnostics next jump=true<CR>", desc = "Diagnostics [n]ext" },
+		{ "<M-p>", "<Cmd>Trouble diagnostics prev jump=true<CR>", desc = "Diagnostics [p]revious" },
 	},
+	opts = {},
 	config = function(_, opts)
 		require("trouble").setup(opts)
-		vim.keymap.set("n", "<M-n>", "<Cmd>Trouble diagnostics next jump=true<CR>", { desc = "Diagnostics [n]ext" })
-		vim.keymap.set("n", "<M-p>", "<Cmd>Trouble diagnostics prev jump=true<CR>", { desc = "Diagnostics [p]revious" })
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			desc = "Bind LSP functions to Trouble windows",
+			callback = function(args)
+				vim.keymap.set(
+					"n",
+					"gO",
+					"<cmd>Trouble symbols open focus=true<CR>",
+					{ buffer = args.buf, desc = "vim.lsp.buf.document_symbol()" }
+				)
+			end,
+		})
 	end,
 }
