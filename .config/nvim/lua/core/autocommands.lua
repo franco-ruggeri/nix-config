06edc44ed8d976a7123dc082807084c1f272e146
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-23 22:42:03 +0200

    refactor(nvim): remove unnecessary utils
    Now the LSP setup is shared between NullLs and Neovim LSP client, so we
    don't need to put LSP stuff in utils.

--- .config/nvim/lua/core/autocommands.lua
+++ .config/nvim/lua/core/autocommands.lua
@@ -2,6 +2,40 @@ local utils = require("utils")
 
 local M = {}
 
+local function get_format_filter(buffer)
+	local use_null_ls = false
+	local lsp_clients = vim.lsp.get_clients({ bufnr = buffer })
+
+	for i = 1, #lsp_clients do
+		local lsp_client = lsp_clients[i]
+
+		if lsp_client.name == "null-ls" then
+			local null_ls = require("null-ls")
+			local sources = null_ls.get_source({
+				filetype = vim.bo[buffer].filetype,
+				method = null_ls.methods.FORMATTING,
+			})
+
+			if #sources > 0 then
+				use_null_ls = true
+			end
+			break
+		end
+	end
+
+	-- We assume there can be only two LSP servers providing formatting: null-ls and another one.
+	-- If there is a null-ls formatter, we use only that to solve conflicts.
+	if use_null_ls then
+		return function(client)
+			return client.name == "null-ls"
+		end
+	else
+		return function()
+			return true
+		end
+	end
+end
+
 M.setup = function()
 	vim.api.nvim_create_autocmd("TextYankPost", {
 		desc = "Highlight when copying text",
@@ -13,8 +47,21 @@ M.setup = function()
 	vim.api.nvim_create_autocmd("LspAttach", {
 		desc = "Set keymaps and autocommands",
 		callback = function(args)
-			utils.lsp.set_keymaps(args.buf)
-			utils.lsp.set_autocommands(args.buf)
+			local function map(mode, key, action, desc)
+				vim.keymap.set(mode, key, action, { buffer = args.buf, desc = desc })
+			end
+			map("n", "gD", vim.lsp.buf.declaration, "[g]oto [d]eclaration")
+
+			-- Format on save
+			-- Multiple LSP servers might provide formatting.
+			-- To avoid conflicts, we use a filter so that only one of them go through.
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = vim.api.nvim_create_augroup("my-lsp-format", { clear = false }),
+				buffer = args.buf,
+				callback = function()
+					vim.lsp.buf.format({ filter = get_format_filter(args.buf) })
+				end,
+			})
 		end,
 	})
 end
--- .config/nvim/lua/utils/init.lua
+++
@@ -1,5 +0,0 @@
-local M = {}
-
-M.lsp = require("utils.lsp")
-
-return M
