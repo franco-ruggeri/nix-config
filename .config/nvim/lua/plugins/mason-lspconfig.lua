3914a976aa8c8075af123017ffa14820abe31abe
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-23 19:08:01 +0200

    chore(nvim): remove unnecessary option (default)

--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -4,7 +4,5 @@ return {
 		"williamboman/mason.nvim", -- package manager for LSP servers
 		"neovim/nvim-lspconfig", -- default configurations for LSP clients
 	},
-	opts = {
-		automatic_enable = true,
-	},
+	opts = {},
 }
