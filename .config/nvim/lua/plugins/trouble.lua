211337623d825764f21f05b02ddc5643a09411a9
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-24 00:27:24 +0200

    refactor(nvim): simplify gO remap in trouble

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,7 +1,7 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	event = { "FileType", "BufWinEnter" },
+	event = { "LspAttach" },
 	cmd = "Trouble",
 	keys = {
 		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics toggle" },
@@ -9,11 +9,10 @@ return {
 		{ "<M-p>", "<Cmd>Trouble diagnostics prev jump=true<CR>", desc = "Diagnostics [p]revious" },
 	},
 	opts = {
-		open_no_results = true, -- good for layout
+		open_no_results = true, -- useful to open windows just for layout
 		modes = {
 			symbols = {
-				focus = true, -- same behavior as original `gO`
-				win = { position = "left" }, -- good to have code more in the center
+				win = { position = "left" }, -- better than right, so code gets more centered
 			},
 		},
 	},
@@ -21,24 +20,15 @@ return {
 		local trouble = require("trouble")
 		trouble.setup(opts)
 
-		vim.api.nvim_create_autocmd("FileType", {
-			desc = "Set Trouble document symbol keymap",
+		vim.api.nvim_create_autocmd("LspAttach", {
+			desc = "Bind LSP functions to Trouble",
 			callback = function(args)
-				-- In help and man, gO opens the outline in a location list, so we don't want to set the keymap.
-				if vim.tbl_contains({ "help", "man" }, args.match) then
-					return
-				end
-
-				-- In the rest of the filetypes, gO either calls vim.lsp.buf.document_symbol() or does nothing.
-				-- We set the keymap to open the Trouble document symbol list.
-				--
-				-- This way:
-				-- - In buffers with LSP clients, we get the same behavior as in the default gO keymap.
-				-- - In buffers without LSP clients, we can still open the window to have a consistent layout.
+				-- To get the same behavior as the default gO, we need to open the window with focus.
+				-- Trouble takes care of calling vim.lsp.buf.document_symbol().
 				vim.keymap.set(
 					"n",
 					"gO",
-					"<cmd>Trouble symbols open<CR>",
+					"<cmd>Trouble symbols open focus=true<CR>",
 					{ buffer = args.buf, desc = "[g]oto [o]utline (document symbols)" }
 				)
 			end,
