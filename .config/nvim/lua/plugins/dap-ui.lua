7451e9654747faa144917614d9e7e6cbda3989c7
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-24 01:05:04 +0200

    fix(nvim): don't mess up layout on debugging
    Manage DAP UI in a separate tab, keeping only the current buffer.

--- .config/nvim/lua/plugins/dap-ui.lua
+++ .config/nvim/lua/plugins/dap-ui.lua
@@ -1,3 +1,30 @@
+local is_open = false
+
+-- Manage DAP UI in a separate tab, so that the window layout is not affected.
+-- The DAP UI tab will have only one window with the current buffer (cleaner).
+local function open_dapui()
+	local buffer = vim.api.nvim_get_current_buf()
+	vim.cmd("tabnew")
+	vim.api.nvim_set_current_buf(buffer)
+	require("dapui").open()
+	is_open = true
+end
+
+local function close_dapui()
+	require("dapui").close()
+	vim.cmd("tabclose")
+	is_open = false
+end
+
+local function toggle_dapui()
+	print("toggling")
+	if is_open then
+		close_dapui()
+	else
+		open_dapui()
+	end
+end
+
 return {
 	"rcarriga/nvim-dap-ui",
 	dependencies = {
@@ -5,15 +32,12 @@ return {
 		"mfussenegger/nvim-dap",
 	},
 	config = function()
-		local dapui = require("dapui")
-		dapui.setup()
+		require("dapui").setup()
 
 		local dap = require("dap")
-		dap.listeners.before.attach.dapui_config = dapui.open
-		dap.listeners.before.launch.dapui_config = dapui.open
-		dap.listeners.before.event_terminated.dapui_config = dapui.close
-		dap.listeners.before.event_exited.dapui_config = dapui.close
+		dap.listeners.before.launch.dapui_config = open_dapui
+		dap.listeners.before.event_terminated.dapui_config = close_dapui
 
-		vim.keymap.set("n", "<Leader>du", dapui.toggle, { desc = "[d]ebug [U]I toggle" })
+		vim.keymap.set("n", "<Leader>du", toggle_dapui, { desc = "[d]ebug [U]I toggle" })
 	end,
 }
