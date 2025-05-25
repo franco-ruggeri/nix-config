0898e39486807d25bd334083a833b4eb015132f6
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-25 17:02:35 +0200

    feat(nvim): add --force option in install script

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Trick to drop existing files, if present
-stow --target=$HOME --no-folding --adopt .
-git restore .
+# Overwrite existing files
+if [[ "$1" == "--force" ]]; then
+	stow --target=$HOME --no-folding --adopt .
+	git restore .
+fi
 
-# Actual installation
 stow --target=$HOME --no-folding .
+if [[ $? -ne 0 ]]; then
+	echo "Tip: Use --force to overwrite existing files."
+	exit 1
+fi
