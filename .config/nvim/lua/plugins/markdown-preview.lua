1f3952c311ed48a4b4ac9fe41a1a0d5a095c348a
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-27 00:43:03 +0200

    chore(nvim): add TODO

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -2,6 +2,7 @@ return {
 	"iamcco/markdown-preview.nvim",
 	build = ":call mkdp#util#install()",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	ft = { "markdown" }, -- TODO: need to understnad why without this it doesn't work
 	keys = {
 		{ "<leader>mp", "<Cmd>MarkdownPreviewToggle<CR>", desc = "[m]arkdown [p]review toggle" },
 	},
