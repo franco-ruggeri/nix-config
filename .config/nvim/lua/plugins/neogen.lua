a9c1babefd91a452b2af345dd95faafd776860b6
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-28 17:25:58 +0200

    feat(nvim): lazy-load neogen

--- .config/nvim/lua/plugins/neogen.lua
+++ .config/nvim/lua/plugins/neogen.lua
@@ -1,10 +1,9 @@
 return {
 	"danymat/neogen",
+	keys = {
+		{ "<leader>ga", "<Cmd>Neogen<CR>", desc = "[g]enerate [a]nnotation" },
+	},
 	opts = {
 		snippet_engine = "nvim", -- use built-in snippet engine (:h vim.snippet)
 	},
-	config = function(_, opts)
-		require("neogen").setup(opts)
-		vim.keymap.set("n", "<leader>ga", "<Cmd>Neogen<CR>", { desc = "[g]enerate [a]nnotation" })
-	end,
 }
