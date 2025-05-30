f3c474c3255e72460704571ddf1ec2affcd998d5
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-30 12:53:23 +0200

    fix(nvim): use latest commit for plenary.nvim
    Copilot chat gets stuck with the latest stable version of plenary.nvim,
    but not with the latest commit.
    
    See https://github.com/CopilotC-Nvim/CopilotChat.nvim/issues/1054

--- .config/nvim/lua/plugins/copilot-chat.lua
+++ .config/nvim/lua/plugins/copilot-chat.lua
@@ -3,7 +3,10 @@ return {
 	enabled = true,
 	dependencies = {
 		"zbirenbaum/copilot.lua", -- required
-		"nvim-lua/plenary.nvim", -- required
+		{
+			"nvim-lua/plenary.nvim", -- required
+			version = false, -- latest commit, see https://github.com/CopilotC-Nvim/CopilotChat.nvim/issues/1054
+		},
 	},
 	build = "make tiktoken",
 	opts = {},
