949b6b9de981a4ca8a9d06b9d1c5635abe737929
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-06-03 23:06:40 +0200

    feat(nvim): disable pylsp linting and formatting
    For Python, I use external linters and formatters. From pylsp, I want
    other LSP methods but not linting and formatting.

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,3 +1,18 @@
 return {
 	"neovim/nvim-lspconfig",
+	config = function()
+		vim.lsp.config("pylsp", {
+			settings = {
+				pylsp = {
+					plugins = {
+						pyflakes = { enabled = false },
+						autopep8 = { enabled = false },
+						mccabe = { enabled = false },
+						pycodestyle = { enabled = false },
+						yapf = { enabled = false },
+					},
+				},
+			},
+		})
+	end,
 }
