f4e739111f0b9298fa80f9a7fee40008eb9660ca
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-06-06 01:29:58 +0200

    chore(nvim): remove latex-preview (wrapped as a plugin)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,1 @@
 require("core").setup()
-require("extra").setup()
--- .config/nvim/lua/extra/init.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-M.latex_preview = require("extra.latex-preview")
-
-M.setup = function()
-	M.latex_preview.setup()
-end
-
-return M
