f65110724621fd9a4a53319323b866d7194afe6e
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-06-08 13:35:20 +0200

    feat(nvim): set port for markdown preview

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -4,9 +4,10 @@ return {
 	ft = { "markdown" },
 	init = function()
 		-- In SSH sessions, port forwarding can be used to access the preview.
-		-- We need to print the URL and make the server listen on all interfaces.
-		vim.g.mkdp_echo_preview_url = 1
-		vim.g.mkdp_open_to_the_world = 1
+		-- For this use case, we need to...
+		vim.g.mkdp_port = 5000 -- ... use a predefined port
+		vim.g.mkdp_open_to_the_world = 1 -- ... make the server listen on all interfaces
+		vim.g.mkdp_echo_preview_url = 1 -- ... print the URL for confirmation (for convenience)
 	end,
 	config = function()
 		vim.cmd([[
@@ -15,15 +16,15 @@ return {
     endfunction
     ]])
 
+		-- In SSH sessions, we do not want to open the browser on the server.
 		vim.keymap.set("n", "<leader>mp", function()
+			vim.g.mkdp_browserfunc = "Noop"
+			vim.cmd("MarkdownPreviewToggle")
+		end, { desc = "[m]arkdown [p]review toggle" })
+
+		vim.keymap.set("n", "<leader>mP", function()
 			vim.g.mkdp_browserfunc = ""
 			vim.cmd("MarkdownPreviewToggle")
 		end, { desc = "[m]arkdown [p]review toggle (with browser)" })
-
-		-- In SSH sessions, we might not want to open the browser.
-		vim.keymap.set("n", "<leader>mP", function()
-			vim.g.mkdp_browserfunc = "Noop"
-			vim.cmd("MarkdownPreviewToggle")
-		end, { desc = "[m]arkdown [p]review toggle (without browser)" })
 	end,
 }
--- .config/nvim/lua/plugins/pdf-preview.lua
+++ .config/nvim/lua/plugins/pdf-preview.lua
@@ -6,10 +6,10 @@ return {
 	},
 	opts = {
 		pdf_filepath = "build/main.pdf",
+		port = 5001,
 	},
 	config = function(_, opts)
 		require("pdf-preview").setup(opts)
-
 		vim.keymap.set("n", "<leader>lP", "<Cmd>PdfPreviewStop<CR>", { desc = "[l]atex [p]review stop" })
 	end,
 }
