5dc97210f04f93ed02758c04de300c705e24ffa9
Franco Ruggeri <francoruggeri95@gmail.com>
2025-06-23 22:04:04 +0200

    feat(nvim): add keymap to open tests with focus

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -1,3 +1,16 @@
+local function open_and_focus()
+	vim.cmd("Neotest summary open")
+
+	for _, win in ipairs(vim.api.nvim_list_wins()) do
+		local buf = vim.api.nvim_win_get_buf(win)
+		local ft = vim.bo[buf].filetype
+		if ft == "neotest-summary" then
+			vim.api.nvim_set_current_win(win)
+			break
+		end
+	end
+end
+
 return {
 	"nvim-neotest/neotest",
 	dependencies = {
@@ -8,6 +21,9 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		-- Adapters
 		"nvim-neotest/neotest-python",
+	},
+	keys = {
+		{ "<leader>te", open_and_focus, desc = "[te]st list" },
 	},
 	config = function()
 		---@diagnostic disable-next-line: missing-fields
