e32c4faec1a045f04f8027dc0e8f7070817a905d
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-06-24 13:33:20 +0000

    feat(nvim): customize completion menu
    - kind name (useful until I familiarize with the icons).
    - source name (useful to understand where the suggestion comes from).

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -20,5 +20,24 @@ return {
 				},
 			},
 		},
+		completion = {
+			menu = {
+				draw = {
+					columns = {
+						{ "label", "label_description", gap = 1 },
+						{ "kind_icon", "kind", gap = 1 },
+						{ "source_name" },
+					},
+					components = {
+						source_name = {
+							text = function(ctx)
+								return "[" .. ctx.source_name .. "]"
+							end,
+							highlight = "BlinkCmpLabel",
+						},
+					},
+				},
+			},
+		},
 	},
 }
