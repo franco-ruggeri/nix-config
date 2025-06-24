3d25517a2ec71c467f1c6631ac7dd5340d81523b
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-06-24 11:22:36 +0000

    feat(nvim): add keymap for running last task

--- .config/nvim/lua/plugins/overseer.lua
+++ .config/nvim/lua/plugins/overseer.lua
@@ -19,4 +19,18 @@ return {
 			direction = "left",
 		},
 	},
+	config = function(_, opts)
+		local overseer = require("overseer")
+		overseer.setup(opts)
+
+		vim.keymap.set("n", "<leader>taR", function()
+			-- Recipe from https://github.com/stevearc/overseer.nvim/blob/master/doc/recipes.md#restart-last-task
+			local tasks = overseer.list_tasks({ recent_first = true })
+			if vim.tbl_isempty(tasks) then
+				vim.notify("No tasks found", vim.log.levels.WARN)
+			else
+				overseer.run_action(tasks[1], "restart")
+			end
+		end, { desc = "[ta]sk [r]un last" })
+	end,
 }
