03b30cd77eeab2090b84a572c525838d39e6fc78
Franco Ruggeri <francoruggeri95@gmail.com>
2025-06-26 11:53:16 +0200

    Revert "fix(nvim): re-enable basic linting via pylsp"
    This reverts commit 6e52c7cd48ae5972b33c89232c11652ad787c12f.

--- .config/nvim/lsp/pylsp.lua
+++ .config/nvim/lsp/pylsp.lua
@@ -1,9 +1,14 @@
 return {
 	settings = {
 		pylsp = {
+			-- For Python, standard formatters and linters are available with null-ls.
+			-- So, disable diagnostics and formatting plugins to avoid conflicts with them.
 			plugins = {
-				autopep8 = { enabled = false }, -- formatting via null-ls
-				pycodestyle = { enabled = false },  -- linting via null-ls
+				pyflakes = { enabled = false },
+				autopep8 = { enabled = false },
+				mccabe = { enabled = false },
+				pycodestyle = { enabled = false },
+				yapf = { enabled = false },
 			},
 		},
 	},
