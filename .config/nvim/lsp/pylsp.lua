6e52c7cd48ae5972b33c89232c11652ad787c12f
Franco Ruggeri <francoruggeri95@gmail.com>
2025-06-26 11:27:12 +0200

    fix(nvim): re-enable basic linting via pylsp
    When external linters aren't available, there should be basic
    diagnostics via the LSP server (e.g. module not found).

--- .config/nvim/lsp/pylsp.lua
+++ .config/nvim/lsp/pylsp.lua
@@ -1,14 +1,9 @@
 return {
 	settings = {
 		pylsp = {
-			-- For Python, standard formatters and linters are available with null-ls.
-			-- So, disable diagnostics and formatting plugins to avoid conflicts with them.
 			plugins = {
-				pyflakes = { enabled = false },
-				autopep8 = { enabled = false },
-				mccabe = { enabled = false },
-				pycodestyle = { enabled = false },
-				yapf = { enabled = false },
+				autopep8 = { enabled = false }, -- formatting via null-ls
+				pycodestyle = { enabled = false },  -- linting via null-ls
 			},
 		},
 	},
