5012bd2d794ff08c95b407f07579a1d98037d3ba
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-06-29 11:23:15 +0200

    Revert "chore(nvim): use default pdf-preview options"
    This reverts commit 294122826f7fed415dff851b0d6615e4641d688c.

--- .config/nvim/lua/plugins/pdf-preview.lua
+++ .config/nvim/lua/plugins/pdf-preview.lua
@@ -1,5 +1,15 @@
 return {
 	"franco-ruggeri/pdf-preview.nvim",
 	dev = true,
-  opts = {},
+	keys = {
+		{ "<leader>lp", "<Cmd>PdfPreviewStart<CR>", desc = "[l]atex [p]review start" },
+	},
+	opts = {
+		pdf_filepath = "build/main.pdf",
+		port = 5001,
+	},
+	config = function(_, opts)
+		require("pdf-preview").setup(opts)
+		vim.keymap.set("n", "<leader>lP", "<Cmd>PdfPreviewStop<CR>", { desc = "[l]atex [p]review stop" })
+	end,
 }
