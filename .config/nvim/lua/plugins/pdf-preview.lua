294122826f7fed415dff851b0d6615e4641d688c
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-06-29 11:20:46 +0200

    chore(nvim): use default pdf-preview options

--- .config/nvim/lua/plugins/pdf-preview.lua
+++ .config/nvim/lua/plugins/pdf-preview.lua
@@ -1,15 +1,5 @@
 return {
 	"franco-ruggeri/pdf-preview.nvim",
 	dev = true,
-	keys = {
-		{ "<leader>lp", "<Cmd>PdfPreviewStart<CR>", desc = "[l]atex [p]review start" },
-	},
-	opts = {
-		pdf_filepath = "build/main.pdf",
-		port = 5001,
-	},
-	config = function(_, opts)
-		require("pdf-preview").setup(opts)
-		vim.keymap.set("n", "<leader>lP", "<Cmd>PdfPreviewStop<CR>", { desc = "[l]atex [p]review stop" })
-	end,
+  opts = {},
 }
