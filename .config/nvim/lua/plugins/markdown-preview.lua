a85e3d86c6c87fea4c5ff18ceb25fe8a981c4443
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-06-29 10:12:21 +0000

    chore(nvim): minor change in comments

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -11,7 +11,7 @@ return {
     ]])
 		vim.g.mkdp_browserfunc = "Noop" -- ... disable browser opening
 		vim.g.mkdp_port = 5050 -- ... use always the same port (easier to forward)
-		vim.g.mkdp_echo_preview_url = 1 -- ... print the URL (to confirm which port is used)
+		vim.g.mkdp_echo_preview_url = 1 -- ... print the URL (to know link to page)
 	end,
 	config = function()
 		vim.keymap.set("n", "<leader>mp", "<Cmd>MarkdownPreviewToggle<CR>", { desc = "[m]arkdown [p]review toggle" })
