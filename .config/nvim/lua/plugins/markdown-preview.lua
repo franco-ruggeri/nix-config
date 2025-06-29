587700bd5fe280c87d0330f263aa7ad6b2a1cb16
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-06-29 09:54:40 +0000

    refactor(nvim): simplify markdown-preview config

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -3,23 +3,16 @@ return {
 	build = ":call mkdp#util#install()",
 	ft = { "markdown" },
 	init = function()
-		-- In SSH sessions, port forwarding can be used to access the preview.
-		-- For this use case, we need to...
-		vim.g.mkdp_port = 5000 -- ... use a predefined port
-		vim.g.mkdp_open_to_the_world = 1 -- ... make the server listen on all interfaces
-		vim.g.mkdp_echo_preview_url = 1 -- ... print the URL
+		-- Since I mainly use Neovim over SSH...
+		vim.cmd([[
+      function Noop(url)
+      " Do nothing
+      endfunction
+    ]])
+		vim.g.mkdp_browserfunc = "Noop" -- ... disable browser opening
+		vim.g.mkdp_echo_preview_url = 1 -- ... print the URL (to know which port is used)
 	end,
 	config = function()
-		vim.cmd([[
-    function Noop(url)
-    " Do nothing
-    endfunction
-    ]])
-
-		vim.keymap.set("n", "<leader>mp", function()
-			vim.g.mkdp_browserfunc = "Noop" -- in SSH sessions, we do not want to open the browser on the server
-			vim.cmd("MarkdownPreview")
-		end, { desc = "[m]arkdown [p]review start" })
-		vim.keymap.set("n", "<leader>mP", "<Cmd>MarkdownPreviewStop<CR>", { desc = "[m]arkdown [p]review stop" })
+		vim.keymap.set("n", "<leader>mp", "<Cmd>MarkdownPreviewToggle<CR>", { desc = "[m]arkdown [p]review toggle" })
 	end,
 }
