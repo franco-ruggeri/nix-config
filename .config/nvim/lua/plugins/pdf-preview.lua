82ede879a0c0645239a27db932ad164d01cab94c
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-06-29 11:43:23 +0200

    feat(nvim): lazy-load pdf-preview and add keymap

--- .config/nvim/lua/plugins/pdf-preview.lua
+++ .config/nvim/lua/plugins/pdf-preview.lua
@@ -1,15 +1,9 @@
 return {
 	"franco-ruggeri/pdf-preview.nvim",
 	dev = true,
+  cmd = { "PdfPreviewStart", "PdfPreviewToggle" },
 	keys = {
-		{ "<leader>lp", "<Cmd>PdfPreviewStart<CR>", desc = "[l]atex [p]review start" },
+		{ "<leader>p", "<Cmd>PdfPreviewToggle<CR>", desc = "[P]DF preview toggle" },
 	},
-	opts = {
-		pdf_filepath = "build/main.pdf",
-		port = 5001,
-	},
-	config = function(_, opts)
-		require("pdf-preview").setup(opts)
-		vim.keymap.set("n", "<leader>lP", "<Cmd>PdfPreviewStop<CR>", { desc = "[l]atex [p]review stop" })
-	end,
+  opts = {},
 }
