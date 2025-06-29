0e41d555b4059d1bd08481db57312d19d9e5e285
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-06-29 10:10:08 +0000

    feat(nvim): use specific port for markdown-preview

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -10,7 +10,8 @@ return {
       endfunction
     ]])
 		vim.g.mkdp_browserfunc = "Noop" -- ... disable browser opening
-		vim.g.mkdp_echo_preview_url = 1 -- ... print the URL (to know which port is used)
+		vim.g.mkdp_port = 5050 -- ... use always the same port (easier to forward)
+		vim.g.mkdp_echo_preview_url = 1 -- ... print the URL (to confirm which port is used)
 	end,
 	config = function()
 		vim.keymap.set("n", "<leader>mp", "<Cmd>MarkdownPreviewToggle<CR>", { desc = "[m]arkdown [p]review toggle" })
--- .config/nvim/lua/plugins/pdf-preview.lua
+++ .config/nvim/lua/plugins/pdf-preview.lua
@@ -1,9 +1,9 @@
 return {
 	"franco-ruggeri/pdf-preview.nvim",
 	dev = true,
-  cmd = { "PdfPreviewStart", "PdfPreviewToggle" },
+	cmd = { "PdfPreviewStart", "PdfPreviewToggle" },
 	keys = {
 		{ "<leader>p", "<Cmd>PdfPreviewToggle<CR>", desc = "[P]DF preview toggle" },
 	},
-  opts = {},
+	opts = {},
 }
