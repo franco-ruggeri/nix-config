5ef3f59c2873c889e03baa4b5048b31a7b6ff6f6
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-06-30 14:03:07 +0200

    refactor(nvim): move lsp/ config to lspconfig
    lsp/ has a lower priority to the order in runtimepath. So, conflicting
    configs would take the lspconfig settings. The recommended way to
    override lspconfig is to call vim.lsp.config().

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,3 +1,38 @@
 return {
 	"neovim/nvim-lspconfig",
+	-- Warning: Don't use `lsp/<name>.lua`. lspconfig would have precedence when merging settings.
+	-- To override lspconfig settings, use `vim.lsp.config()` here.
+	config = function()
+		vim.lsp.config("pylsp", {
+			settings = {
+				pylsp = {
+					-- For Python, standard formatters and linters are available with null-ls.
+					-- So, disable diagnostics and formatting plugins to avoid conflicts with them.
+					plugins = {
+						pyflakes = { enabled = false },
+						autopep8 = { enabled = false },
+						mccabe = { enabled = false },
+						pycodestyle = { enabled = false },
+						yapf = { enabled = false },
+					},
+				},
+			},
+		})
+
+		vim.lsp.config("texlab", {
+			settings = {
+				texlab = {
+					build = {
+						-- texlab supports compiling the project in two ways:
+						-- * With a custom LSP method.
+						-- * With the onSave option.
+						--
+						-- Neovim's built-in LSP client does not support the custom LSP method for building.
+						-- So, we use the onSave option. The project is compiled on save.
+						onSave = true,
+					},
+				},
+			},
+		})
+	end,
 }
