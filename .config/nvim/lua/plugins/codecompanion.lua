a97c25af8290528c5cec1468fa5ff4a326ac52f4
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-03 15:21:55 +0000

    WIP: configure codecompanion

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -1,20 +1,23 @@
 return {
 	"olimorris/codecompanion.nvim",
-	enabled = false, -- TODO: trying avante
-	version = false,
+	event = "VeryLazy",
 	dependencies = {
 		{
 			"nvim-lua/plenary.nvim", -- required
 			version = false, -- latest commit, see https://codecompanion.olimorris.dev/installation.html
 		},
 		"nvim-treesitter/nvim-treesitter", -- required
-		"ravitemer/mcphub.nvim", -- MCP integration
-		"MeanderingProgrammer/render-markdown.nvim", -- markdown rendering
+		"MeanderingProgrammer/render-markdown.nvim", -- for rendering chat
+		"ravitemer/mcphub.nvim", -- for integration with mcphub.nvim
 		"zbirenbaum/copilot.lua", -- for copilot authentication
 	},
 	opts = {
 		strategies = {
 			chat = {
+				adapter = {
+					name = "copilot",
+					model = "gemini-2.5-pro",
+				},
 				keymaps = {
 					-- Swap close and stop for consistency with edgy.nvim
 					close = { modes = { n = "q" } },
@@ -27,18 +30,17 @@ return {
 						},
 					},
 				},
-				slash_commands = {
-					image = {
-						opts = {
-							dirs = { "~/dotfiles/", vim.fn.getcwd() },
-						},
-					},
-				},
 				opts = {
 					-- TODO: need to open it in the current buffer (the one indicated by #{buffer}... how?
 					-- by default it opens a new tab, I don't want that
 					-- See https://codecompanion.olimorris.dev/configuration/chat-buffer.html#jump-action
 					goto_file_action = "edit",
+				},
+				roles = {
+					llm = function(adapter)
+						return "󱜙 " .. adapter.formatted_name
+					end,
+					user = " User",
 				},
 			},
 		},
@@ -62,9 +64,9 @@ return {
 		require("codecompanion").setup(opts)
 		-- TODO: check other commands and add the useful ones
 		-- TODO: add commands
-		--
-		vim.keymap.set({ "n", "v" }, "<Leader>aa", "<Cmd>CodeCompanionActions<CR>", { desc = "[A]I [a]ctions" })
-		vim.keymap.set("n", "<Leader>ac", "<Cmd>CodeCompanionChat<CR>", { desc = "[A]I [c]hat" })
-		vim.keymap.set("v", "<Leader>ac", "<Cmd>CodeCompanionChat Add<CR>", { desc = "[A]I [c]hat" })
+		-- TODO: change keymaps
+		vim.keymap.set({ "n", "v" }, "<Leader>cc", "<Cmd>CodeCompanionActions<CR>", { desc = "[A]I [a]ctions" })
+		-- vim.keymap.set("n", "<Leader>ac", "<Cmd>CodeCompanionChat<CR>", { desc = "[A]I [c]hat" })
+		-- vim.keymap.set("v", "<Leader>ac", "<Cmd>CodeCompanionChat Add<CR>", { desc = "[A]I [c]hat" })
 	end,
 }
