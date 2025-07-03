fb99b0c48c8fedb05da69df563f8d9f43d8359e4
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-03 09:19:08 +0000

    chore(nvim): simplify status bar
    Removed encoding and fileformat (not that useful) to make space for LSP
    status and MCPHub status. Including everything makes the status bar too
    long and ugly.

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,22 +8,19 @@ return {
 		options = {
 			globalstatus = true,
 		},
-		sections = {
-			lualine_c = { "filename", "lsp_status" }, -- + LSP status
-		},
 		tabline = {
 			lualine_a = { "tabs" },
 			lualine_b = { "filename" },
 		},
 	},
 	config = function(_, opts)
-		local lualine = require("lualine")
-
-		-- + MCPHub status
-		local lualine_x = lualine.get_config().sections.lualine_x
-		table.insert(lualine_x, 1, { require("mcphub.extensions.lualine") })
-		opts.sections.lualine_x = lualine_x
-
-		lualine.setup(opts)
+		opts.sections = {
+			lualine_x = {
+				"lsp_status",
+				"filetype",
+				{ require("mcphub.extensions.lualine") },
+			},
+		}
+		require("lualine").setup(opts)
 	end,
 }
