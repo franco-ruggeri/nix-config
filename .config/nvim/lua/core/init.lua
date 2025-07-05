d947348167e33b6b203e7eec84586e9e4dc0476d
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-05 17:10:29 +0000

    refactor(nvim): rename autocommands -> autocmds

--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -2,13 +2,13 @@ local M = {}
 
 M.options = require("core.options")
 M.keymaps = require("core.keymaps")
-M.autocommands = require("core.autocommands")
+M.autocmds = require("core.autocmds")
 M.plugins = require("core.plugins")
 
 M.setup = function()
 	M.options.setup()
 	M.keymaps.setup()
-	M.autocommands.setup()
+	M.autocmds.setup()
 	M.plugins.setup()
 end
 
