ab91a6917fc894df0045852316c598607f509a6d
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-06 18:30:00 +0000

    feat(nvim): add codecompanion-spinner

---
+++ .config/nvim/lua/plugins/codecompanion-spinner.lua
@@ -0,0 +1,11 @@
+return {
+	"franco-ruggeri/codecompanion-spinner.nvim",
+	dev = true,
+	lazy = true,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		-- log_level = "debug",
+	},
+}
--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -1,101 +1,3 @@
--- Adapted from https://github.com/olimorris/codecompanion.nvim/discussions/640#discussioncomment-12866279
--- TODO: consider wrapping it into a plugin
--- ====================
-local spinners = {}
-
-local Spinner = {}
-
-function Spinner:new(buffer)
-	local object = {
-		buffer = buffer,
-		namespace_id = vim.api.nvim_create_namespace("CodeCompanionSpinner"),
-		processing = false,
-		spinner_index = 1,
-		timer = nil,
-		filetype = "codecompanion",
-		spinner_symbols = {
-			"⠋",
-			"⠙",
-			"⠹",
-			"⠸",
-			"⠼",
-			"⠴",
-			"⠦",
-			"⠧",
-			"⠇",
-			"⠏",
-		},
-	}
-	self.__index = self
-	setmetatable(object, self)
-
-	vim.api.nvim_create_autocmd("User", {
-		pattern = "CodeCompanionRequest*",
-		callback = function(request)
-			if request.buf == self.buffer then
-				if request.match == "CodeCompanionRequestStarted" then
-					self:start()
-				elseif request.match == "CodeCompanionRequestFinished" then
-					self:stop()
-				end
-			end
-		end,
-	})
-
-	return object
-end
-
-function Spinner:update()
-	if not self.processing then
-		self:stop()
-		return
-	end
-
-	vim.api.nvim_buf_clear_namespace(self.buffer, self.namespace_id, 0, -1)
-	self.spinner_index = (self.spinner_index % #self.spinner_symbols) + 1
-	local last_line = vim.api.nvim_buf_line_count(self.buffer) - 1
-	vim.api.nvim_buf_set_extmark(self.buffer, self.namespace_id, last_line, 0, {
-		virt_lines = { { { self.spinner_symbols[self.spinner_index] .. " Processing...", "Comment" } } },
-		virt_lines_above = true, -- false means below the line
-	})
-end
-
-function Spinner:start()
-	print("started")
-
-	self.processing = true
-	self.spinner_index = 0
-
-	if self.timer then
-		self.timer:stop()
-		self.timer:close()
-		self.timer = nil
-	end
-
-	self.timer = vim.uv.new_timer()
-	self.timer:start(
-		0,
-		100,
-		vim.schedule_wrap(function()
-			self:update()
-		end)
-	)
-end
-
-function Spinner:stop()
-	print("stopped")
-	self.processing = false
-
-	if self.timer then
-		self.timer:stop()
-		self.timer:close()
-		self.timer = nil
-	end
-
-	vim.api.nvim_buf_clear_namespace(self.buffer, self.namespace_id, 0, -1)
-end
--- ====================
-
 return {
 	"olimorris/codecompanion.nvim",
 	dependencies = {
@@ -108,6 +10,7 @@ return {
 		"ravitemer/mcphub.nvim", -- for MCP servers
 		"zbirenbaum/copilot.lua", -- for copilot authentication
 		"echasnovski/mini.diff", -- for cleaner diff with @{insert_edit_into_file}
+		"franco-ruggeri/codecompanion-spinner.nvim", -- for spinner
 	},
 	keys = {
 		{
@@ -135,7 +38,7 @@ return {
 			copilot = function() -- select default model
 				return require("codecompanion.adapters").extend("copilot", {
 					schema = {
-						model = { default = "claude-3.7-sonnet" },
+						model = { default = "claude-3.7-sonner" },
 					},
 				})
 			end,
@@ -175,27 +78,5 @@ return {
 		-- The rose-pine colorscheme highlights Identifiers as normal text.
 		-- We change to make variables highlighted differently from text.
 		vim.api.nvim_set_hl(0, "CodeCompanionChatVariable", { link = "@tag.attribute" })
-
-		vim.api.nvim_create_autocmd("FileType", {
-			desc = "Create CodeCompanion spinner",
-			pattern = "codecompanion",
-			callback = function(args)
-				print("create spinner for ", args.buf)
-				spinners[args.buf] = Spinner:new(args.buf)
-			end,
-		})
-
-		-- TODO: doesnt work... bufdelete gets called when I type #{buffer}
-		vim.api.nvim_create_autocmd("BufDelete", {
-			desc = "Clear CodeCompanion spinner",
-			callback = function(args)
-				print("buf delete ", args.buf)
-				if spinners[args.buf] then
-					print(vim.inspect(spinners[args.buf]))
-					spinners[args.buf]:stop()
-					spinners[args.buf] = nil
-				end
-			end,
-		})
 	end,
 }
