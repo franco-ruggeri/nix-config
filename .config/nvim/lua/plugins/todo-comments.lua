2b8dde5075dded020983a832d34e84d1602bcb58
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-08 10:32:49 +0000

    style(nvim): show lightbulb as sign

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -4,6 +4,6 @@ return {
 		"nvim-lua/plenary.nvim", -- required
 	},
 	opts = {
-		sign_priority = 1000, -- override signs from linters (e.g. pylint)
+		sign_priority = 1000, -- show sign above diagnostic ones
 	},
 }
