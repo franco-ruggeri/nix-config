c2b15bc26f577db7ad84fd971acf2c60b38d08e3
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-09 17:17:08 +0200

    chore(nvim): remove avante-neotree integration
    It breaks lazy-loading, and the same integration can't be achieved with
    CodeCompanion, so it would be inconsistent. Not worth it.

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,25 +1,3 @@
-local function avante_add_file(state)
-	local node = state.tree:get_node()
-	local filepath = node:get_id()
-	local relative_path = require("avante.utils").relative_path(filepath)
-	local sidebar = require("avante").get()
-
-	-- Opne Avante sidebar if not already open
-	local open = sidebar:is_open()
-	if not open then
-		require("avante.api").ask()
-		sidebar = require("avante").get()
-	end
-
-	-- Add the selected file to the Avante sidebar
-	sidebar.file_selector:add_selected_file(relative_path)
-
-	-- Remove neo tree buffer
-	if not open then
-		sidebar.file_selector:remove_selected_file("neo-tree filesystem [1]")
-	end
-end
-
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
@@ -27,7 +5,6 @@ return {
 		"nvim-lua/plenary.nvim", -- required
 		"MunifTanjim/nui.nvim", -- required
 		"nvim-tree/nvim-web-devicons", -- for file icons
-		"yetone/avante.nvim", -- for Avante integration
 	},
 	opts = {
 		filesystem = {
@@ -35,11 +12,6 @@ return {
 			filtered_items = {
 				hide_dotfiles = false,
 				hide_gitignored = false,
-			},
-			-- Neo-tree integration for adding files to Avante context
-			-- Based on https://github.com/yetone/avante.nvim?tab=readme-ov-file#neotree-shortcut
-			commands = {
-				avante_add_file = avante_add_file,
 			},
 			window = {
 				mappings = {
