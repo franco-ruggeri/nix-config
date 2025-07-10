feeed76d64c93868a558b9f4948199ed3f1f4308
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-10 11:27:46 +0000

    style(nvim): improve select menu for neogit

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -3,11 +3,14 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim", -- required
 		"sindrets/diffview.nvim", -- see modifications
-		"nvim-telescope/telescope.nvim", -- for better menu UI
 	},
 	cmd = "Neogit",
 	keys = {
 		{ "<Leader>gg", "<Cmd>Neogit<CR>", desc = "[g]it status" },
 	},
-	opts = {},
+	opts = {
+		integrations = {
+			telescope = false, -- use vim.select.ui() (=> telescope-ui-select)
+		},
+	},
 }
