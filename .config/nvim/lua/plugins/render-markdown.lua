4105374324ac66e656b6d089cc3108d73e1fabd9
Franco Ruggeri <francoruggeri95@gmail.com>
2025-07-13 02:04:19 +0200

    style(nvim): update render markdown style
    - In headings, disable only background and icons, not everything.
    - In dash separators, set width to 80.

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -12,12 +12,17 @@ return {
 		completions = {
 			lsp = { enabled = true },
 		},
-		heading = { enabled = false }, -- no background color for headings
+		heading = { -- no background color for headings
+			backgrounds = false, -- no background color
+			icons = false, -- no icons
+		},
 		code = {
 			highlight = false, -- no background color for code blocks
 			highlight_border = false, -- no background color for code info line
 		},
-		-- ====================
+		dash = {
+			width = 80, -- same as textwidth of markdown files
+		},
 	},
 	config = function(_, opts)
 		require("render-markdown").setup(opts)
