ba8303bdd2229ba103f09433f0e3911fb10dd5ef
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-25 01:30:17 +0200

    feat(nvim): add markdown image toggle, drop chafa

--- .config/nvim/lua/plugins/image.lua
+++ .config/nvim/lua/plugins/image.lua
@@ -1,15 +1,8 @@
-local backends = {
-	"kitty", -- high-fidelity, not working in nested tmux and dev containers
-	"ueberzug", -- low-fidelity, working everywhere
-}
-local backend_idx = 1
-
 return {
 	"3rd/image.nvim",
 	version = false,
 	build = false,
 	opts = {
-		backend = backends[backend_idx],
 		processor = "magick_cli",
 		tmux_show_only_in_active_window = true,
 		integrations = {
@@ -22,12 +15,22 @@ return {
 		local image = require("image")
 		image.setup(opts)
 
-		local function change_backend()
-			backend_idx = (backend_idx % #backends) + 1
-			opts.backend = backends[backend_idx]
-			image.clear()
+		local enabled = true
+		local function toggle_markdown()
+			enabled = not enabled
+			if enabled then
+				opts.integrations.markdown.filetypes = { "markdown", "md" }
+			else
+				-- Disable markdown integration by the removing the filetypes associated with it.
+				--
+				-- Note that setting opts.integrations.markdown.enabled=false does not work.
+				-- That flag is used for loading the integration and cannot be undone dynamically.
+				opts.integrations.markdown.filetypes = {}
+			end
 			image.setup(opts)
 		end
-		vim.api.nvim_create_user_command("ImageBackendChange", change_backend, {})
+
+		vim.api.nvim_create_user_command("MarkdownImageToggle", toggle_markdown, {})
+		vim.keymap.set("n", "<leader>mi", "<Cmd>MarkdownImageToggle<CR>", { desc = "[m]arkdown [i]mage toggle" })
 	end,
 }
