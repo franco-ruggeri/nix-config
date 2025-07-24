1e6dd8b5b659c7f4a7b0c894b16a3b7b55d26ba6
Franco Ruggeri <francoruggeri95@gmail.com>
2025-07-25 00:54:55 +0200

    feat(nvim): add command to change image backend

--- .config/nvim/lua/plugins/image.lua
+++ .config/nvim/lua/plugins/image.lua
@@ -1,8 +1,15 @@
+local backends = {
+	"kitty", -- high-fidelity, not working in nested tmux and dev containers
+	"ueberzug", -- low-fidelity, working everywhere
+}
+local backend_idx = 1
+
 return {
 	"3rd/image.nvim",
 	version = false,
 	build = false,
 	opts = {
+		backend = backends[backend_idx],
 		processor = "magick_cli",
 		tmux_show_only_in_active_window = true,
 		integrations = {
@@ -11,4 +18,16 @@ return {
 			},
 		},
 	},
+	config = function(_, opts)
+		local image = require("image")
+		image.setup(opts)
+
+		local function change_backend()
+			backend_idx = (backend_idx % #backends) + 1
+			opts.backend = backends[backend_idx]
+			image.clear()
+			image.setup(opts)
+		end
+		vim.api.nvim_create_user_command("ImageBackendChange", change_backend, {})
+	end,
 }
