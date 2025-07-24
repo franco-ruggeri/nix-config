2466b0ca37f753b5100f6a324ea3608a6bd283aa
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-24 03:18:36 +0200

    feat(nvim): add obsidian.nvim

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,43 @@
+local vault_path = vim.fn.expand("~/codebase/notes")
+
+return {
+	"obsidian-nvim/obsidian.nvim",
+	event = {
+		("BufReadPre %s/*.md"):format(vault_path),
+		("BufNewFile %s/*.md"):format(vault_path),
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"MeanderingProgrammer/render-markdown.nvim", -- for better rendering
+		"folke/snacks.nvim", -- for image viewing
+	},
+	opts = {
+		-- TODO: create new note in 0-inbox
+		workspaces = {
+			{
+				name = "notes",
+				path = vault_path,
+			},
+		},
+		-- TODO: try templates
+		-- TODO: move _assets to .assets
+		templates = {
+			folder = "_assets/templates",
+		},
+		attachments = {
+			img_folder = "_assets/attachments",
+		},
+		completion = { -- marksman takes care of completion via LSP
+			nvim_cmp = false,
+			blink = false,
+		},
+		ui = {
+			enable = false, -- render-markdown takes care of nice rendering
+		},
+	},
+	config = function(_, opts)
+		require("obsidian").setup(opts)
+
+		-- TODO: set keymaps
+	end,
+}
