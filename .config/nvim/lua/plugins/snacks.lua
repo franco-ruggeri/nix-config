25673735383d66e6e14c6131945012e3f9e817cc
Franco Ruggeri <francoruggeri95@gmail.com>
2025-07-25 03:01:18 +0200

    refactor(nvim): don't lazy load snacks

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,5 @@
 return {
 	"folke/snacks.nvim",
-	lazy = true, -- load on demand (e.g., only in certain projects via .nvim.lua)
 	opts = {
 		image = {
 			math = { enabled = false }, -- render-markdown takes care of it
