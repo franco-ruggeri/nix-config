06ea666f20330ba238b8e170ee236f4400233efc
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-25 03:12:38 +0200

    feat(nvim): enable snacks if imagemagick available

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,7 @@ return {
 	"folke/snacks.nvim",
 	opts = {
 		image = {
+			enabled = vim.fn.executable("magick") == 1 or vim.fn.executable("convert") == 1,
 			math = { enabled = false }, -- render-markdown takes care of it
 		},
 	},
