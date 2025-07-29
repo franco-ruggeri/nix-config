d4a3a8ac2a421ec6f7107cebdee2853f5ba16cf2
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-29 20:42:04 +0200

    chore(nvim): always enable snacks.image

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,14 +1,7 @@
-local function should_enable_image()
-	return vim.fn.getenv("GHOSTTY_BIN_DIR") -- ghostty installed
-		and not vim.fn.getenv("SSH_CONNECTION") -- not in SSH session
-		and (vim.fn.executable("magick") == 1 or vim.fn.executable("convert") == 1) -- imagemagick installed
-end
-
 return {
 	"folke/snacks.nvim",
 	opts = {
 		image = {
-			enabled = should_enable_image(),
 			math = { enabled = false }, -- render-markdown takes care of it
 		},
 	},
