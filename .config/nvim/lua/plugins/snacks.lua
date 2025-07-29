c07f4ab69a146deefb5f254616094a58edfed479
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-29 15:02:58 +0200

    fix: pass boolean to enable snacks.image (not fn)

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,11 +1,12 @@
+local function should_enable_image()
+	return vim.fn.getenv("GHOSTTY_BIN_DIR") and (vim.fn.executable("magick") == 1 or vim.fn.executable("convert") == 1)
+end
+
 return {
 	"folke/snacks.nvim",
 	opts = {
 		image = {
-			enabled = function()
-				return vim.fn.getenv("GHOSTTY_BIN_DIR")
-					and (vim.fn.executable("magick") == 1 or vim.fn.executable("convert") == 1)
-			end,
+			enabled = should_enable_image(),
 			math = { enabled = false }, -- render-markdown takes care of it
 		},
 	},
