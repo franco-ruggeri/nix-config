90baeec3c9c6a104cde2382ebd94af1f44034ffc
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-29 11:48:09 +0200

    feat(nvim): enable snacks.img only in ghostty env

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,7 +2,10 @@ return {
 	"folke/snacks.nvim",
 	opts = {
 		image = {
-			enabled = vim.fn.executable("magick") == 1 or vim.fn.executable("convert") == 1,
+			enabled = function()
+				return vim.fn.getenv("GHOSTTY_BIN_DIR")
+					and (vim.fn.executable("magick") == 1 or vim.fn.executable("convert") == 1)
+			end,
 			math = { enabled = false }, -- render-markdown takes care of it
 		},
 	},
