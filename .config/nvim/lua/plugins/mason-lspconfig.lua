802602cf65912caf818ba32a17e0148d748b02c4
Franco Ruggeri <francoruggeri95@gmail.com>
2025-08-01 03:25:41 +0200

    feat(nvim): enable per-project lsp, format, lint
    Now, language servers, formatters, and linters have to be enabled from
    .nvim.lua. So, only the explicitly enabled tools for each project will
    be enabled.
    
    This mechanism allows developing in multiple projects with different
    tools, reducing my dependency on devcontainers.

--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"williamboman/mason-lspconfig.nvim",
-	dependencies = {
-		"williamboman/mason.nvim", -- package manager for language servers
-		"neovim/nvim-lspconfig", -- default configurations for LSP clients
-	},
-	opts = {},
-}
--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,4 +1,52 @@
 return {
 	"nvimtools/none-ls.nvim", -- maintained fork of null-ls
-	opts = {},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup()
+
+		local diagnostics = null_ls.builtins.diagnostics
+		local formatting = null_ls.builtins.formatting
+
+		-- For Python linters, if in Python venv...
+		local python_env = {}
+		local python_prefer_local = nil
+		if vim.env.VIRTUAL_ENV then
+			-- ... set PYTHONPATH so that Mason packages can see Python packages installed in venv
+			python_env = { PYTHONPATH = vim.fn.glob(vim.env.VIRTUAL_ENV .. "/lib/python*/site-packages") }
+			-- ... use venv binary over Mason binary when linter is installed in venv
+			python_prefer_local = vim.env.VIRTUAL_ENV .. "/bin"
+		end
+
+		diagnostics.pylint = diagnostics.pylint.with({
+			env = python_env,
+			prefer_local = python_prefer_local,
+		})
+
+		diagnostics.mypy = diagnostics.mypy.with({
+			env = python_env,
+			prefer_local = python_prefer_local,
+			generator_opts = vim.tbl_extend("force", diagnostics.mypy.generator.opts, {
+				multiple_files = false, -- the default is wrongly true... this source lints one buffer at a time
+			}),
+		})
+
+		-- Specificalized prettier with prose wrap for Markdown files
+		formatting.prettier_markdown = formatting.prettier.with({
+			filetypes = { "markdown" },
+			extra_args = { "--prose-wrap", "always" },
+		})
+
+		-- Remove Markdown from default prettier
+		formatting.prettier = formatting.prettier.with({
+			filetypes = vim.tbl_filter(function(ft)
+				return ft ~= "markdown"
+			end, null_ls.builtins.formatting.prettier.filetypes),
+		})
+
+		-- Prettier wraps lines at 80 where possible. In some cases (e.g., long links), it is not possible.
+		-- Markdownlint would nevertheless complain about these lines. Disable the line-length rule.
+		formatting.markdownlint = formatting.markdownlint.with({
+			extra_args = { "--prose-wrap", "always" },
+		})
+	end,
 }
