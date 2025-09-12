a12d4da02d4d6c0bf341ebc08598aff8fe9e31ce
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-09-12 15:43:35 +0200

    Revert "refactor(nvim): load local config from .nvim/"
    This reverts commit cc9984adf859f79d0a7819163edce7e85bd074ac.

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -42,6 +42,9 @@ M.setup = function()
 	vim.opt.splitright = true
 	vim.opt.splitbelow = true
 
+	-- Enable local config
+	vim.opt.exrc = true
+
 	-- Open buffers without folding
 	vim.opt.foldenable = false
 
