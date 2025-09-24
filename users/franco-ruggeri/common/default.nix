e9749ee995905f35f73ac9c408eacdfe40052547
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-09-24 23:15:19 +0200

    WIP: migrate from Mason to Nix

--- users/franco-ruggeri/common/default.nix
+++ users/franco-ruggeri/common/default.nix
@@ -22,10 +22,6 @@
     discord
     super-productivity
     zoom-us
-
-    # TODO: for some reason, nil_ls fails to compile on macos with mason... 
-    # I need to find a consistent way, either drop Mason for everything or make nil work with mason
-    nil
   ];
 
   programs = {
@@ -46,6 +42,69 @@
     neovim = {
       enable = true;
       defaultEditor = true;
+      extraPackages = with pkgs; [
+        # Lua
+        # lua-language-server # language server
+        # stylua # formatter
+
+        # Bash
+        # bash-language-server # language server
+        # shfmt # formatter
+
+        # Markdown
+        # marksman # language server
+        # markdownlint # linter
+        # prettier # formatter
+
+        # JSON
+        # json-lsp # language server with linter
+        # prettier # formatter
+
+        # YAML
+        # yaml-language-server # language server
+        # prettier # formatter
+
+        # TOML
+        # taplo # language server with formatter
+
+        # XML
+        # lemminx # language server with formatter
+
+        # Helm
+        # helm-ls # language server
+
+        # Docker
+        # dockerfile-language-server # language server
+        # hadolint # linter
+
+        # Python
+        # python-lsp-server # language server
+        # ruff # linter and formatter
+        # pylint # linter (some rules not covered by ruff, see https://github.com/astral-sh/ruff/issues/970)
+        # mypy # linter (static type checker)
+
+        # C/C++
+        # clangd # language server
+        # clang-format # formatter
+        # cpptools # debug adapter
+
+        # TypeScript and JavaScript
+        # typescript-language-server # language server
+        # prettier # formatter
+
+        # LaTeX
+        # texlab # language server with formatter
+
+        # Java
+        # jdtls # language server
+        # google-java-format # formatter
+        # java-debug-adapter # debug adapter
+        # java-test # use debug adapter on tests
+
+        # Nix
+        nil # language server with linter
+        nixfmt-rfc-style # formatter
+      ];
     };
     gpg.enable = true;
     fzf = {
