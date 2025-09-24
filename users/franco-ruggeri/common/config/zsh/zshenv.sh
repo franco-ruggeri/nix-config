69ca51fbcc257a0af2ec9b23163523a6f43b61d2
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-09-24 22:59:33 +0200

    refactor(zsh): put aliases+keybindings in zshrc

--- users/franco-ruggeri/common/config/zsh/zshenv.sh
+++ users/franco-ruggeri/common/config/zsh/zshenv.sh
@@ -1,4 +1,4 @@
-zsh_config_path=$HOME/.config/zsh
+zsh_config_path="$HOME/.config/zsh"
 source $zsh_config_path/utils-init.sh
 
 # Homebrew
@@ -6,17 +6,8 @@ if is_darwin; then
 	eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-# Gnome keyring for ssh-agent.
-# See https://wiki.archlinux.org/title/GNOME/Keyring#Setup_gcr
-#
-# Set SSH_AUTH_SOCK only if it is not already set, to avoid overriding it in remote sessions.
-# See https://wiki.archlinux.org/title/SSH_keys#Forwarding_ssh-agent
-#if is_linux && [ -z "$SSH_AUTH_SOCK" ]; then
-#	export SSH_AUTH_SOCK="/run/user/1000/gcr/ssh"
-#fi
-
 # AIChat
-if [ is_darwin ]; then
+if is_darwin; then
 	export AICHAT_CONFIG_DIR="$HOME/.config/aichat"
 fi
 
--- users/franco-ruggeri/common/config/zsh/zshrc.sh
+++ users/franco-ruggeri/common/config/zsh/zshrc.sh
@@ -1,27 +1,71 @@
-zsh_config_path=$HOME/.config/zsh
-source $zsh_config_path/utils-init.sh
-source $zsh_config_path/aliases.sh
-source $zsh_config_path/keybindings.sh
+zsh_config_dir="$HOME/.config/zsh"
+source $zsh_config_dir/utils-init.sh
 
-# Kubernetes
+# Aliases
+# ====================
+alias ls="ls --color=auto"
+alias ll="ls -lh"
+alias la="ls -lAh"
+alias vi="nvim"
+alias vim="nvim"
+if is_linux; then
+	alias open="xdg-open"
+fi
+
+aichat() {
+	if [ -z $GEMINI_API_KEY ]; then
+		load_secret GEMINI_API_KEY
+	fi
+	command aichat "$@"
+}
+# ====================
+
+# Key bindings
+# ====================
+# Terminal application mode (to make $terminfo valid)
+# See https://wiki.archlinux.org/title/Zsh#Key_bindings
+if [[ -n "$terminfo[smkx]" && -n "$terminfo[rmkx]" ]]; then
+	autoload -Uz add-zle-hook-widget
+	function zle_application_mode_start {
+		echoti smkx
+	}
+	function zle_application_mode_stop {
+		echoti rmkx
+	}
+	add-zle-hook-widget -Uz zle-line-init zle_application_mode_start
+	add-zle-hook-widget -Uz zle-line-finish zle_application_mode_stop
+fi
+
+# Basic key bindings
+bind_key "$terminfo[khome]" beginning-of-line
+bind_key "$terminfo[kend]" end-of-line
+bind_key "$terminfo[kdch1]" delete-char
+bind_key "$terminfo[kcbt]" reverse-menu-complete
+
+# History search of matching commands
+# See https://wiki.archlinux.org/title/Zsh#History_search
+autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bind_key "$terminfo[kcuu1]" up-line-or-beginning-search
+bind_key "$terminfo[kcud1]" down-line-or-beginning-search
+# ====================
+
 if is_command_available kubectl; then
 	source <(kubectl completion zsh)
 fi
 
-# Devpod
 if is_command_available devpod; then
 	source <(devpod completion zsh)
 fi
 
-# AIChat
 if is_command_available aichat; then
 	source <(curl -sSL https://raw.githubusercontent.com/sigoden/aichat/main/scripts/shell-integration/integration.zsh)
 	source <(curl -sSL https://raw.githubusercontent.com/sigoden/aichat/main/scripts/completions/aichat.zsh)
 fi
 
-# Ericsson
 if is_darwin; then
 	source $HOME/.ericsson
 fi
 
-source $zsh_config_path/utils-clear.sh
+source $zsh_config_dir/utils-clear.sh
