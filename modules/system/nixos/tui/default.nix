4f7ab1e24c206d1dd6d1ea0c13be5dedfb616be2
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-10-03 22:41:11 +0200

    chore(nixos): remove user from docker group
    Users in the docker group can bypass security policies and become root.
    Better to avoid issues and just use sudo when needed.

--- modules/system/nixos/tui/default.nix
+++ modules/system/nixos/tui/default.nix
@@ -6,8 +6,6 @@ in
   imports = [ ./kubernetes.nix ];
 
   config = lib.mkIf cfg.enable {
-    users.users.${config.myModules.system.username}.extraGroups = [ "docker" ];
-
     programs = {
       gnupg.agent = {
         enable = true;
