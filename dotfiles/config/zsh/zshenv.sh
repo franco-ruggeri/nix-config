9585709abd6ac5d4b512556f35c206064165cde1
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-10-07 00:18:59 +0200

    fix: add workaround for Nix in devcontainer

--- dotfiles/config/zsh/zshenv.sh
+++ dotfiles/config/zsh/zshenv.sh
@@ -1,12 +1,16 @@
 zsh_config_path="$HOME/.config/zsh"
-source $zsh_config_path/utils-init.sh
+source "$zsh_config_path/utils-init.sh"
 
 if is_darwin; then
-  # Homebrew
+	# Homebrew
 	eval "$(/opt/homebrew/bin/brew shellenv)"
 
-  # AIChat
+	# AIChat
 	export AICHAT_CONFIG_DIR="$HOME/.config/aichat"
 fi
 
-source $zsh_config_path/utils-clear.sh
+# In devcontainers, the Nix feature does not source the Nix profile in Zsh.
+# Thus, we add the binaries to the path explicitly.
+path+=("$HOME/.nix-profile/bin")
+
+source "$zsh_config_path/utils-clear.sh"
