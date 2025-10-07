02b7e19d057105a336a6c5f1b447ba9ad02ab5db
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-10-07 15:26:19 +0200

    chore: update project overlay local config path

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -1,7 +1,8 @@
 require("core").setup()
 
--- Load local config from .personal/.nvim.lua (project overlay)
-local path = vim.fn.getcwd() .. "/.personal/.nvim.lua"
+-- Load local config from project overlay
+-- See https://github.com/franco-ruggeri/project-overlays
+local path = vim.fn.getcwd() .. "/.personal/nvim.lua"
 if vim.secure.read(path) then
 	dofile(path)
 end
