49cc6b3fd2777b5ae3a36d0d4b9786fdb2a786f5
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-10-13 17:54:39 +0200

    chore: add note on wg-quick

--- modules/system/darwin/gui.nix
+++ modules/system/darwin/gui.nix
@@ -8,10 +8,10 @@ in
       casks = [
         "google-drive"
         "adobe-acrobat-reader"
-        # WARNING: The Nix option (services.karabiner-elements) is currently broken.
+        # WARNING: The Nix module (services.karabiner-elements) is currently broken.
         # See https://github.com/nix-darwin/nix-darwin/issues/1041
         "karabiner-elements"
-        # WARNING: The Nix option (programs.ghostty) is currently broken.
+        # WARNING: The Nix module (programs.ghostty) is currently broken.
         # See https://github.com/NixOS/nixpkgs/issues/388984
         "ghostty"
         # WARNING: The Nix package (pkgs.obs-studio) does not support darwin currently.
@@ -21,6 +21,7 @@ in
         "wireshark-app"
       ];
       masApps = {
+        # WARNING: The Nix module (networking.wg-quick) has problems in autostarting the launchd service.
         "WireGuard" = 1451685025;
       };
     };
