a292c9343ee9291876b1c26964047385064dee54
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-10-15 19:06:39 +0200

    fix: set WG allowedIPs differently on k8s servers

--- modules/system/nixos/wireguard.nix
+++ modules/system/nixos/wireguard.nix
@@ -3,6 +3,17 @@
 let
   cfg = config.myModules.system.wireguard;
   listenPort = 51820;
+  kubernetes = config.myModules.system.kubernetes.enable;
+  # * On Kubernetes, route only specific traffic to avoid loops.
+  # * Outside Kubernetes, route all traffic through the VPN.
+  allowedIPs =
+    lib.optionals kubernetes [
+      "10.34.0.0/24" # VPN
+      "10.43.0.0/16" # K8s cluster
+    ]
+    ++ lib.optionals (!kubernetes) [
+      "0.0.0.0/0" # all traffic
+    ];
 in
 {
   options.myModules.system.wireguard = {
@@ -24,11 +35,7 @@ in
             {
               publicKey = "PqMzcV9O8M/X6EkM9OETa065Vg1mTHWaikbQR5Z55Ro=";
               presharedKeyFile = cfg.presharedKeyFile;
-              allowedIPs = [
-                "10.34.0.0/24" # VPN
-                "10.43.0.0/16" # Kubernetes cluster
-                "192.168.1.30/32" # Kubernetes API server
-              ];
+              allowedIPs = allowedIPs;
               endpoint = "ruggeri.asuscomm.com:51820";
               persistentKeepalive = 15;
             }
