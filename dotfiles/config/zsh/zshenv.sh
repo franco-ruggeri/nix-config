402b4f398e5964c36f75b91a1baa54a28cb48920
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-10-16 22:48:26 +0200

    feat(nix-darwin): add pngpaste

--- dotfiles/config/zsh/zshenv.sh
+++ dotfiles/config/zsh/zshenv.sh
@@ -13,8 +13,10 @@ fi
 # Thus, we add the binaries to the path explicitly.
 path+=("$HOME/.nix-profile/bin")
 
+# Set XDG_SESSION_TYPE in Hyprland.
+# Needed for obsidian.nvim img_paste to work properly.
 if is_linux && [ "$XDG_CURRENT_DESKTOP" = "Hyprland" ]; then
-	export XDG_SESSION_TYPE="wayland" # used by obsidian.nvim img_paste
+	export XDG_SESSION_TYPE="wayland"
 fi
 
 source "$zsh_config_path/utils-clear.sh"
--- modules/home/darwin/tui.nix
+++ modules/home/darwin/tui.nix
@@ -1,11 +1,16 @@
 {
   config,
+  pkgs,
   lib,
   myLib,
   ...
 }:
 {
   config = lib.mkIf (myLib.isDarwin && config.myModules.home.tui.enable) {
+    home.packages = with pkgs; [
+      pngpaste # for obsidian.nvim image pasting
+    ];
+
     programs.gpg.enable = true;
   };
 }
