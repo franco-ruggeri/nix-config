9e89ba453df2a8aeb469f5f58073266320c29cfc
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2026-01-20 23:09:37 +0100

    chore(hypr): remove startup command for openrgb
    The server is already started with the NixOS module.

--- modules/system/nixos/gui.nix
+++ modules/system/nixos/gui.nix
@@ -28,6 +28,8 @@ in
       hardware.openrgb = {
         enable = true;
         package = pkgs.openrgb-with-all-plugins;
+        # WARNING: enable when I upgrade to NixOS 25.11
+        # startupProfile = "off";
       };
       # WARNING: The home-manager module (services.gnome-keyring) does not work.
       # See https://github.com/nix-community/home-manager/issues/1454
