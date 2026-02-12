3920e9f797b2a40c21cb8860d5893f5f1c31bbb7
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2026-02-12 10:36:04 +0100

    fix(nvim): disable copilot in dap-repl

--- dotfiles/config/nvim/lua/plugins/copilot.lua
+++ dotfiles/config/nvim/lua/plugins/copilot.lua
@@ -6,5 +6,10 @@ return {
 		suggestion = {
 			auto_trigger = true,
 		},
+		filetypes = {
+			-- Avoid errors in DAP REPL buffers.
+			-- See https://github.com/rcarriga/nvim-dap-ui/issues/102
+			["dap-repl"] = false,
+		},
 	},
 }
