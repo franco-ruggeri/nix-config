eb2b53121402756023d2a8663f760f92ef4a8fb9
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2026-02-13 22:19:01 +0100

    fix(nfs): add dependency on zfs-mount.service
    It is required for correctly bind-mounting ZFS datasets that are mounted
    with non-legacy mounts.
    
    See https://wiki.archlinux.org/title/ZFS#Bind_mount

--- modules/system/nixos/nfs-server.nix
+++ modules/system/nixos/nfs-server.nix
@@ -1,3 +1,4 @@
+# Assumptions: The user has created a ZFS dataset named k8s-nfs with mountpoint=/mnt/k8s-nfs.
 { config, lib, ... }:
 let
   cfg = config.myModules.system.nfs.server;
@@ -8,31 +9,46 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = config.myModules.system.zfs.enable;
+        message = "ZFS must be enabled for NFS server.";
+      }
+    ];
+
     networking.firewall.allowedTCPPorts = [ 2049 ];
 
-    fileSystems = {
-      "/srv/nfs/k8s-nfs" = {
-        device = "/mnt/zfs/k8s-nfs";
-        options = [ "bind" ];
+    fileSystems =
+      let
+        options = [
+          "bind"
+          "nofail"
+          "x-systemd.requires=zfs-mount.service"
+        ];
+      in
+      {
+        "/srv/nfs/k8s-nfs" = {
+          device = "/mnt/zfs/k8s-nfs";
+          inherit options;
+        };
+        "/srv/nfs/k8s-nfs-ro" = {
+          device = "/mnt/zfs/k8s-nfs";
+          inherit options;
+        };
       };
-      "/srv/nfs/k8s-nfs-backup" = {
-        device = "/mnt/zfs/k8s-nfs";
-        options = [ "bind" ];
-      };
-    };
 
     # NFS exports:
-    # * k8s-nfs: read-write, available for the K8s nodes.
-    # * k8s-nfs-backup: read-only, available for the backup servers.
+    # * k8s-nfs: read-write access from K8s nodes.
+    # * k8s-nfs-ro: read-only access from backup servers.
     #
-    # no_root_squash is needed for NextCloud to work property.
+    # Note: no_root_squash is needed for NextCloud to work property.
     # See https://github.com/nextcloud/helm/issues/588
     services.nfs.server = {
       enable = true;
       exports = ''
         /srv/nfs 10.34.0.0/24(ro,fsid=0)
         /srv/nfs/k8s-nfs 10.34.0.2/32(rw,no_root_squash)
-        /srv/nfs/k8s-nfs-backup 10.34.0.0/24(ro)
+        /srv/nfs/k8s-nfs-ro 10.34.0.3/24 10.34.0.5/24(ro) 10.34.0.6/24(ro)
       '';
     };
   };
