026737c4a7645351e4faa4120517f86e4641edac
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2026-02-17 23:52:19 +0100

    fix(nfs): back up only latest snapshot
    Backing up all snapshots will cause full duplications, as restic sees
    them as distinct directories. Backing up only the latest snapshot
    leverages restic incremental snapshots.

--- scripts/nfs-backup.sh
+++ scripts/nfs-backup.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-BACKUP_PATH="$NFS_MOUNT_POINT/k8s-backup"
-
 if ! mount | grep -q "$NFS_MOUNT_POINT"; then
 	echo "NFS export not mounted at $NFS_MOUNT_POINT."
 	exit 1
@@ -14,8 +12,23 @@ if ! restic cat config; then
 	restic init
 fi
 
+echo "Preparing backup environment..."
+TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+BACKUP_PATH="/tmp/k8s-backup-$TIMESTAMP"
+mkdir -p "$BACKUP_PATH"
+cd "$BACKUP_PATH" # relative paths for restic
+
+echo "Linking latest snapshots..."
+link_latest() {
+	local dir="$1"
+	local src
+	src=$(find "$NFS_MOUNT_POINT/k8s-backup/$dir" -mindepth 1 -maxdepth 1 -type d | sort | tail -n1)
+	ln -s "$src" "$dir"
+}
+link_latest "nfs"
+link_latest "longhorn"
+
 echo "Starting restic backup..."
-cd "$BACKUP_PATH"
 restic backup .
 
 echo "Pruning old snapshots..."
