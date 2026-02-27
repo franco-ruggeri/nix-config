71fc2aaa7251c7c72b445d329d0c19ed78468aa0
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2026-02-27 18:48:06 +0100

    fix(homelab): correct commands in launchd agents

--- modules/home/darwin/homelab-backup.nix
+++ modules/home/darwin/homelab-backup.nix
@@ -15,10 +15,11 @@ let
     # See https://forum.restic.net/t/backing-up-zfs-snapshots-good-idea/9604
     RESTIC_FEATURES = "device-id-for-hardlinks";
   };
-  scriptSecrets = ''
+  wrapScript = script: ''
     export RESTIC_REPOSITORY_FILE=${cfg.resticRepositoryFile} && \
     export RESTIC_PASSWORD_FILE=${config.age.secrets.restic-password.path} && \
-    export SMTP_PASSWORD_FILE=${config.age.secrets.smtp-password.path}
+    export SMTP_PASSWORD_FILE=${config.age.secrets.smtp-password.path} && \
+    ${script}
   '';
   pythonScriptDir = myLib.mkPythonScriptDir {
     derivationName = "homelab_test_backup_daily";
@@ -41,88 +42,104 @@ in
     # In EnvironmentVariables, the home-manager command to get the agenix path would not be expanded.
     # So we have to export the environment variables with agenix secrets in ProgramArguments.
     launchd.agents = {
-      homelab-make-backup = {
-        enable = true;
-        config = {
-          Label = "org.nixos.homelab-make-backup";
-          ProgramArguments = [
-            "bash"
-            "-c"
-            "${scriptSecrets} && ${myLib.mkShellScript "homelab-make-backup.sh"}"
-          ];
-          StartCalendarInterval = [
-            {
-              Hour = 14;
-              Minute = 0;
-            }
-          ];
-          EnvironmentVariables = environmentVariables;
-          StandardOutPath = "${config.home.homeDirectory}/Library/Logs/homelab-make-backup/out.log";
-          StandardErrorPath = "${config.home.homeDirectory}/Library/Logs/homelab-make-backup/error.log";
+      homelab-make-backup =
+        let
+          script = wrapScript (myLib.mkShellScript "homelab-make-backup.sh");
+        in
+        {
+          enable = true;
+          config = {
+            Label = "org.nixos.homelab-make-backup";
+            ProgramArguments = [
+              "bash"
+              "-c"
+              "${script}"
+            ];
+            StartCalendarInterval = [
+              {
+                Hour = 14;
+                Minute = 0;
+              }
+            ];
+            EnvironmentVariables = environmentVariables;
+            StandardOutPath = "${config.home.homeDirectory}/Library/Logs/homelab-make-backup/out.log";
+            StandardErrorPath = "${config.home.homeDirectory}/Library/Logs/homelab-make-backup/error.log";
+          };
         };
-      };
-      homelab-test-backup-daily = {
-        enable = true;
-        config = {
-          Label = "org.nixos.homelab-test-backup-daily";
-          ProgramArguments = [
-            "bash"
-            "-c"
-            "${scriptSecrets} && ${pythonScriptDir}/homelab_test_backup_daily.py"
-          ];
-          StartCalendarInterval = [
-            {
-              Hour = 13;
-              Minute = 0;
-            }
-          ];
-          EnvironmentVariables = environmentVariables;
-          StandardOutPath = "${config.home.homeDirectory}/Library/Logs/homelab-test-backup-daily/out.log";
-          StandardErrorPath = "${config.home.homeDirectory}/Library/Logs/homelab-test-backup-daily/error.log";
+      homelab-test-backup-daily =
+        let
+          script = wrapScript "${pythonScriptDir}/homelab_test_backup_daily.py";
+        in
+        {
+          enable = true;
+          config = {
+            Label = "org.nixos.homelab-test-backup-daily";
+            ProgramArguments = [
+              "bash"
+              "-c"
+              "${script}"
+            ];
+            StartCalendarInterval = [
+              {
+                Hour = 13;
+                Minute = 0;
+              }
+            ];
+            EnvironmentVariables = environmentVariables;
+            StandardOutPath = "${config.home.homeDirectory}/Library/Logs/homelab-test-backup-daily/out.log";
+            StandardErrorPath = "${config.home.homeDirectory}/Library/Logs/homelab-test-backup-daily/error.log";
+          };
         };
-      };
-      homelab-test-backup-weekly = {
-        enable = true;
-        config = {
-          Label = "org.nixos.homelab-test-backup-weekly";
-          ProgramArguments = [
-            "bash"
-            "-c"
-            "${scriptSecrets} && ${pythonScriptDir}/homelab_test_backup_weekly.py"
-          ];
-          StartCalendarInterval = [
-            {
-              Weekday = 1;
-              Hour = 13;
-              Minute = 0;
-            }
-          ];
-          EnvironmentVariables = environmentVariables;
-          StandardOutPath = "${config.home.homeDirectory}/Library/Logs/homelab-test-backup-weekly/out.log";
-          StandardErrorPath = "${config.home.homeDirectory}/Library/Logs/homelab-test-backup-weekly/error.log";
+      homelab-test-backup-weekly =
+        let
+          script = wrapScript "${pythonScriptDir}/homelab_test_backup_weekly.py";
+        in
+        {
+          enable = true;
+          config = {
+            Label = "org.nixos.homelab-test-backup-weekly";
+            ProgramArguments = [
+              "bash"
+              "-c"
+              "${script}"
+            ];
+            StartCalendarInterval = [
+              {
+                Weekday = 1;
+                Hour = 13;
+                Minute = 0;
+              }
+            ];
+            EnvironmentVariables = environmentVariables;
+            StandardOutPath = "${config.home.homeDirectory}/Library/Logs/homelab-test-backup-weekly/out.log";
+            StandardErrorPath = "${config.home.homeDirectory}/Library/Logs/homelab-test-backup-weekly/error.log";
+          };
         };
-      };
-      homelab-test-backup-monthly = {
-        enable = true;
-        config = {
-          Label = "org.nixos.homelab-test-backup-monthly";
-          ProgramArguments = [
-            "bash"
-            "-c"
-            "${scriptSecrets} && ${pythonScriptDir}/homelab_test_backup_monthly.py"
-          ];
-          StartCalendarInterval = [
-            {
-              Day = 1;
-              Hour = 13;
-              Minute = 0;
-            }
-          ];
-          EnvironmentVariables = environmentVariables;
-          StandardOutPath = "${config.home.homeDirectory}/Library/Logs/homelab-test-backup-monthly/out.log";
-          StandardErrorPath = "${config.home.homeDirectory}/Library/Logs/homelab-test-backup-monthly/error.log";
+      homelab-test-backup-monthly =
+        let
+          script = wrapScript "${pythonScriptDir}/homelab_test_backup_monthly.py";
+        in
+        {
+          enable = true;
+          config = {
+            Label = "org.nixos.homelab-test-backup-monthly";
+            ProgramArguments = [
+              "bash"
+              "-c"
+              "${script}"
+            ];
+            StartCalendarInterval = [
+              {
+                Day = 1;
+                Hour = 13;
+                Minute = 0;
+              }
+            ];
+            EnvironmentVariables = environmentVariables;
+            StandardOutPath = "${config.home.homeDirectory}/Library/Logs/homelab-test-backup-monthly/out.log";
+            StandardErrorPath = "${config.home.homeDirectory}/Library/Logs/homelab-test-backup-monthly/error.log";
+          };
         };
-      };
     };
 
     age.secrets = myLib.mkSecrets [
