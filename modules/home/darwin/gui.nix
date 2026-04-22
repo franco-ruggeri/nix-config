0dca54b54d9f2197aadee717cbd9f46cb132930a
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2026-04-22 20:49:21 +0200

    revert: remove hack for zen-mode in aerospace
    This reverts commit 8a1675e4d007f24170f184abcf1183959f8e3f9e.
    
    Too annoying given that it works globally, not per-app or per-workspace.

--- modules/home/darwin/gui.nix
+++ modules/home/darwin/gui.nix
@@ -24,25 +24,8 @@ in
       };
     };
 
-    xdg.configFile =
-      myLib.mkConfigDotfiles [
-        "karabiner"
-      ]
-      # HACK: Zen-mode in AeroSpace. Remove when AeroSpace implements it.
-      # See https://github.com/nikitabobko/AeroSpace/discussions/2061
-      # ====================
-      # "aerospace-zen"
-      // {
-        "aerospace-zen/toggle.sh" = {
-          source = myLib.dotfilesConfigDir + "/aerospace-zen/toggle.sh";
-          executable = true;
-        };
-      }
-      // {
-        "aerospace-zen/aerospace.toml" = {
-          source = myLib.dotfilesConfigDir + "/aerospace-zen/aerospace.toml";
-        };
-      };
-    # ====================
+    xdg.configFile = myLib.mkConfigDotfiles [
+      "karabiner"
+    ];
   };
 }
