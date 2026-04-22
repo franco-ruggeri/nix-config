c78d3f2531dc163a7285462db5ae0b33f91f17b5
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2026-04-22 22:24:58 +0200

    style(nvim): open aerial in floating window

--- dotfiles/config/nvim/lua/plugins/aerial.lua
+++ dotfiles/config/nvim/lua/plugins/aerial.lua
@@ -6,13 +6,12 @@ return {
 	},
 	opts = {
 		layout = {
-			-- Since the window is managed by edgy.nvim, if `resize_to_content` is true:
-			-- * The first time, the width gets resized to fit the content.
-			-- * The second time (or after a zoom), the width gets adjusted by edgy.nvim.
-			--
-			-- Essentially, there would be inconsistency and an ugly flicker at the beginning.
-			-- Setting it to false avoids that.
-			resize_to_content = false,
+			default_direction = "float",
+			min_width = 0.5,
+		},
+		float = {
+			relative = "editor",
+			min_height = 0.5,
 		},
 	},
 	config = function(_, opts)
@@ -25,12 +24,10 @@ return {
 				-- * For help and man buffers, gO opens the outline in a location list. We want to keep that behavior.
 				-- * For buffers with LSP clients attached, gO calls `vim.lsp.buf.document_symbol()`.
 				--  As a result, the outline is opened in a location list. We want to change that to open Aerial.
-				-- * For other filetypes, gO does nothing. We want to open Aerial anyway, as it's nice to have it open for layout reasons.
+				-- * For other filetypes, gO does nothing. We want to open Aerial anyway.
 				--
 				-- Aerial takes care of calling `vim.lsp.buf.document_symbol()`. So, it's enough to open it.
-				if not vim.tbl_contains({ "help", "man" }, args.match) then
-					vim.keymap.set("n", "gO", "<Cmd>AerialOpen<CR>", { buffer = args.buf, desc = "[g]oto [o]utline" })
-				end
+				vim.keymap.set("n", "gO", "<Cmd>AerialOpen<CR>", { buffer = args.buf, desc = "[g]oto [o]utline" })
 			end,
 		})
 	end,
