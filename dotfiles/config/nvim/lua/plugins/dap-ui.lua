e8453341886c9970701e2656dba22f29bd51479e
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2026-04-23 09:55:42 +0200

    refactor(nvim): simplify dap-ui config (defaults)
    Now that I dropped edgy, I can use the default open/close process of
    dap-ui, without hacking around it with tabs.

--- dotfiles/config/nvim/lua/plugins/dap-ui.lua
+++ dotfiles/config/nvim/lua/plugins/dap-ui.lua
@@ -11,37 +11,19 @@ return {
 
 		dapui.setup()
 
-		-- Manage DAP UI in a separate tab, so that the window layout is not affected.
-		-- The DAP UI tab will have only one window with the current buffer (cleaner).
-		local function close_dapui()
-			-- On DAP UI close, we return to the first tab and close all other tabs.
-			-- We avoid storing the DAP UI status, as the tab can be closed manually,
-			-- bypassing the DAP UI close function.
-			vim.cmd("tabfirst")
-			vim.cmd("tabonly")
-		end
-		local function open_dapui()
-			close_dapui() -- support opening DAP UI multiple times
-
-			local buffer = vim.api.nvim_get_current_buf()
-			vim.cmd("tabnew")
-			vim.api.nvim_set_current_buf(buffer)
-			dapui.open()
-		end
-
 		-- Some debug adapters do not support termination (e.g., OpenDebugAD7 for C++).
 		-- In those cases, the termination event is not emitted and the DAP UI does not
 		-- get closed. Thus, we close it explicitly.
 		local function terminate_session()
 			require("dap").terminate()
-			close_dapui()
+			dapui.close()
 		end
 
-		dap.listeners.before.launch.dapui_config = open_dapui
-		dap.listeners.before.event_terminated.dapui_config = close_dapui
+		dap.listeners.before.launch.dapui_config = dapui.open
+		dap.listeners.before.event_terminated.dapui_config = dapui.close
 
-		vim.keymap.set("n", "<Leader>du", open_dapui, { desc = "[d]ebug [U]I open" })
-		vim.keymap.set("n", "<Leader>dU", close_dapui, { desc = "[d]ebug [U]I close" })
+		vim.keymap.set("n", "<Leader>du", dapui.open, { desc = "[d]ebug [U]I open" })
+		vim.keymap.set("n", "<Leader>dU", dapui.close, { desc = "[d]ebug [U]I close" })
 		vim.keymap.set("n", "<Leader>dt", terminate_session, { desc = "[d]ebug [t]erminate" })
 	end,
 }
