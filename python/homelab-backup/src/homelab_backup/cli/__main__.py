635bd6fc08eaba09f05195cd817c7734ccad3521
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2026-04-27 00:21:37 +0200

    refactor(homelab-backup): rename dst-* CLI args to dest-*

--- python/homelab-backup/src/homelab_backup/cli/__main__.py
+++ python/homelab-backup/src/homelab_backup/cli/__main__.py
@@ -7,14 +7,14 @@ from homelab_backup.cli import dest_rsync, dest_zfs, source
 def main() -> None:
     logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser(prog="homelab-backup")
-    parser.add_argument("command", choices=["src", "dst-zfs", "dst-rsync"])
+    parser.add_argument("command", choices=["src", "dest-zfs", "dest-rsync"])
     args = parser.parse_args()
 
     if args.command == "src":
         source.main()
-    elif args.command == "dst-zfs":
+    elif args.command == "dest-zfs":
         dest_zfs.main()
-    elif args.command == "dst-rsync":
+    elif args.command == "dest-rsync":
         dest_rsync.main()
     else:
         raise ValueError(f"Unknown command: {args.command}")
