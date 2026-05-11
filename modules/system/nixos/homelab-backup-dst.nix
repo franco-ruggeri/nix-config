2c5d0275a9f169b0a025511fccb7b522324978e9
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2026-05-11 16:47:05 +0200

    fix(homelab-backup): add restic to systemPackages on dst host

--- modules/system/nixos/homelab-backup-dst.nix
+++ modules/system/nixos/homelab-backup-dst.nix
@@ -37,6 +37,8 @@ in
       }
     ];
 
+    environment.systemPackages = with pkgs; [ restic ];
+
     systemd = {
       services.homelab-backup-dst = {
         description = "Homelab backup destination";
