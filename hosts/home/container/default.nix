e2fb62a48a2f5fc055172ce2a2f23af340f63a1b
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2026-05-14 13:49:24 +0200

    feat: add openclaw in container home config

--- hosts/home/container/default.nix
+++ hosts/home/container/default.nix
@@ -1,8 +1,11 @@
+{ pkgs, ... }:
 {
   myModules.home = {
     username = "ubuntu";
     tui.enable = true;
   };
+
+  home.packages = with pkgs; [ openclaw ];
 
   # DO NOT change! Used for backward compatibility.
   home.stateVersion = "25.05";
